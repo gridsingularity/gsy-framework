c15b06da53abcd73f45be842820e2bfb1073a828
Hannes Diedrich <hannes@gridsingularity.com>
2019-07-18 17:36:23 +0200

    D3ASIM-1338: Made the repo installable.

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+
+try:
+    with open('requirements/base.txt') as req:
+        REQUIREMENTS = [r.partition('#')[0] for r in req if not r.startswith('-e')]
+except OSError:
+    # Shouldn't happen
+    REQUIREMENTS = []
+
+with open("README.md", "r") as readme:
+    README = readme.read()
+
+# *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
+VERSION = '1.0.0a0'
+
+setup(
+    name="d3a-interface",
+    description="D3A interface",
+    long_description=README,
+    author='GridSingularity',
+    author_email='d3a@gridsingularity.com',
+    url='https://github.com/gridsingularity/d3a-interface',
+    version=VERSION,
+    packages=find_packages(where="interface"),
+    package_dir={"": "interface"},
+    package_data={},
+    install_requires=REQUIREMENTS,
+    zip_safe=False,
+)
