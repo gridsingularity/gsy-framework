ba4d05ea347e9909262dbadfb26dbaabc12c588d
Hannes Diedrich <hannes@gridsingularity.com>
2020-04-22 09:40:22 +0200

    D3ASIM-1849: Added EXPORT_ENERGY_TRADE_PROFILE_HR flag.

--- d3a_interface/constants_limits.py
+++ d3a_interface/constants_limits.py
@@ -41,6 +41,7 @@ class ConstSettings:
         ENERGY_RATE_DECREASE_PER_UPDATE = 1  # rate decrease in cents_per_update
         SETUP_FILE_PATH = None  # Default path of the available setup files
         EXPORT_DEVICE_PLOTS = True
+        EXPORT_ENERGY_TRADE_PROFILE_HR = False
         # Boolean flag which forces d3a to run in real-time
         RUN_REAL_TIME = False
         KEEP_PAST_MARKETS = False
