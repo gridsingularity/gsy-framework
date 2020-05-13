13720aa624e6eb38e72b3e3f8b3e10f8d37fa9ca
Hannes Diedrich <hannes@gridsingularity.com>
2020-05-13 16:28:09 +0200

    D3ASIM-2349: Adapted area validator to accept the one of the gridfee types to be set to 0.

--- d3a_interface/area_validator.py
+++ d3a_interface/area_validator.py
@@ -19,36 +19,33 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from d3a_interface.constants_limits import ConstSettings, RangeLimit
 from d3a_interface.device_validator import validate_range_limit
 from d3a_interface.exceptions import D3AException
-from d3a_interface.utils import key_in_dict_and_not_none, key_in_dict_and_not_none_and_negative
+from d3a_interface.utils import key_in_dict_and_not_none_and_greater_than_zero, \
+    key_in_dict_and_not_none_and_negative, key_in_dict_and_not_none
 
 
 GeneralSettings = ConstSettings.GeneralSettings
 AreaSettings = ConstSettings.AreaSettings
 
 
-CONSTANT_FEE_LIMIT = RangeLimit(0, 200)
-
-
 def validate_area(**kwargs):
-    is_percentage_fee = key_in_dict_and_not_none(kwargs, "grid_fee_percentage")
-    is_constant_fee = key_in_dict_and_not_none(kwargs, "grid_fee_constant")
-    if is_percentage_fee and is_constant_fee:
+    if key_in_dict_and_not_none_and_greater_than_zero(kwargs, "grid_fee_constant") and \
+            key_in_dict_and_not_none_and_greater_than_zero(kwargs, "grid_fee_percentage"):
         raise D3AException("Cannot set both percentage and constant grid fees on the same area.")
-    if is_percentage_fee:
+    if key_in_dict_and_not_none(kwargs, "grid_fee_percentage"):
         error_message = {"misconfiguration": [f"grid_fee_percentage should be in between "
-                                              f"{AreaSettings.GRID_FEE_PERCENTAGE_LIMIT.min} & "
-                                              f"{AreaSettings.GRID_FEE_PERCENTAGE_LIMIT.max}."]}
-        validate_range_limit(AreaSettings.GRID_FEE_PERCENTAGE_LIMIT.min,
+                                              f"{AreaSettings.PERCENTAGE_FEE_LIMIT.min} & "
+                                              f"{AreaSettings.PERCENTAGE_FEE_LIMIT.max}."]}
+        validate_range_limit(AreaSettings.PERCENTAGE_FEE_LIMIT.min,
                              kwargs["grid_fee_percentage"],
-                             AreaSettings.GRID_FEE_PERCENTAGE_LIMIT.max, error_message)
+                             AreaSettings.PERCENTAGE_FEE_LIMIT.max, error_message)
 
-    elif is_constant_fee:
+    elif key_in_dict_and_not_none(kwargs, "grid_fee_constant"):
         error_message = {"misconfiguration": [f"grid_fee_constant should be in between "
-                                              f"{CONSTANT_FEE_LIMIT.min} & "
-                                              f"{CONSTANT_FEE_LIMIT.max}."]}
-        validate_range_limit(CONSTANT_FEE_LIMIT.min,
+                                              f"{AreaSettings.CONSTANT_FEE_LIMIT.min} & "
+                                              f"{AreaSettings.CONSTANT_FEE_LIMIT.max}."]}
+        validate_range_limit(AreaSettings.CONSTANT_FEE_LIMIT.min,
                              kwargs["grid_fee_constant"],
-                             CONSTANT_FEE_LIMIT.max, error_message)
+                             AreaSettings.CONSTANT_FEE_LIMIT.max, error_message)
 
     if key_in_dict_and_not_none_and_negative(kwargs, "baseline_peak_energy_import_kWh"):
         raise D3AException({"misconfiguration": [f"baseline_peak_energy_import_kWh must be a "
