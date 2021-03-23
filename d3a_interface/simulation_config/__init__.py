dcf83096b2bb546def73cc705d02ad11cbd32cb7
faizan2590 <faizan2590@gmail.com>
2021-03-23 16:42:27 +0100

    D3ASIM-3218: Adapting to comments.

--- d3a_interface/simulation_config/__init__.py
+++ d3a_interface/simulation_config/__init__.py
@@ -1,11 +1,3 @@
 
-
 class SimulationConfigException(Exception):
     pass
-
-
-def validate_simulation_config(simulation_config):
-    if set(simulation_config.keys()) != \
-            {"username", "name", "uuid", "domain_name",
-             "web_socket_domain_name", "global_settings", "registry"}:
-        raise SimulationConfigException(f"simulation_config doesn't have all the keys")
