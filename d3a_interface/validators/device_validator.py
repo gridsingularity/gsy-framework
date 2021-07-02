f3360e7d00b9fcd088cb69d6f1ce377bff625bce
Pierpaolo Pantone <24alsecondo@gmail.com>
2021-07-02 13:20:08 +0200

    refactor device validator to use ABC class

--- d3a_interface/validators/device_validator.py
+++ d3a_interface/validators/device_validator.py
@@ -13,10 +13,10 @@ the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with this program. If
 not, see <http://www.gnu.org/licenses/>.
 """
-from abc import ABCMeta, abstractmethod
+from abc import ABC, abstractmethod
 
 
-class DeviceValidator(metaclass=ABCMeta):
+class DeviceValidator(ABC):
     """Interface for devices' validator classes."""
 
     @classmethod
