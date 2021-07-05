2319e37212daa9d46d46ca1ea7ca37469ec234dd
Hassan Hammoud <hassan.hammoud754@gmail.com>
2021-07-05 12:21:07 +0300

    D3ASIM-3356: Fix flake check

--- d3a_interface/matching_algorithms/__init__.py
+++ d3a_interface/matching_algorithms/__init__.py
@@ -1,3 +1,9 @@
 from .abstract_matching_algorithm import AbstractMatchingAlgorithm
 from .pay_as_bid_algorithm import PayAsBidMatchingAlgorithm
 from .pay_as_clear_algorithm import PayAsClearMatchingAlgorithm
+
+__all__ = [
+    AbstractMatchingAlgorithm,
+    PayAsBidMatchingAlgorithm,
+    PayAsClearMatchingAlgorithm
+]
