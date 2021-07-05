bd748192c477eb9f7a0897dc3399d060fd28646f
Hassan Hammoud <hassan.hammoud754@gmail.com>
2021-07-05 11:43:58 +0300

    D3ASIM-3356: Move pay as clear matching algorithm to d3a-interface

--- d3a_interface/dataclasses.py
+++ d3a_interface/dataclasses.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 @dataclass
@@ -17,3 +17,26 @@ class BidOfferMatch:
             "selected_energy": self.selected_energy,
             "trade_rate": self.trade_rate
         }
+
+
+@dataclass
+class Clearing:
+    rate: float
+    energy: float
+
+    def serializable_dict(self):
+        return {
+            "rate": self.rate,
+            "energy": self.energy
+        }
+
+
+@dataclass
+class MarketClearingState:
+    cumulative_offers: dict = field(default_factory=dict)
+    cumulative_bids: dict = field(default_factory=dict)
+    clearing: dict = field(default_factory=dict)
+
+    @classmethod
+    def _csv_fields(cls):
+        return "time", "rate [ct./kWh]"
