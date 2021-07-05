ec5c74f02b0210dc15573c9488d9cf8acb5b7047
Hassan Hammoud <hassan.hammoud754@gmail.com>
2021-07-05 17:28:07 +0300

    D3ASIM-3356: pay as clear return serialized bidOfferMatch

--- d3a_interface/matching_algorithms/__init__.py
+++ d3a_interface/matching_algorithms/__init__.py
@@ -1,9 +1,9 @@
 from .abstract_matching_algorithm import AbstractMatchingAlgorithm
-from .pay_as_bid_algorithm import PayAsBidMatchingAlgorithm
-from .pay_as_clear_algorithm import PayAsClearMatchingAlgorithm
+from .pay_as_bid_matching_algorithm import PayAsBidMatchingAlgorithm
+from .pay_as_clear_matching_algorithm import PayAsClearMatchingAlgorithm
 
 __all__ = [
-    AbstractMatchingAlgorithm,
-    PayAsBidMatchingAlgorithm,
-    PayAsClearMatchingAlgorithm
+    "AbstractMatchingAlgorithm",
+    "PayAsBidMatchingAlgorithm",
+    "PayAsClearMatchingAlgorithm"
 ]
