cb925bc2368e0d411295da61bf6b9e52467c9ae3
faizan2590 <faizan2590@gmail.com>
2021-07-09 16:15:43 +0200

    D3ASIM:3464: Replacing unittest.TestCase with pytest for newly added test of SavingsKPI.

--- tests/test_savings_kpi.py
+++ tests/test_savings_kpi.py
@@ -1,30 +1,27 @@
-from unittest import TestCase
+import pytest
 from uuid import uuid4
 
 from d3a_interface.sim_results.kpi import SavingsKPI
 
 
-class TestSavingsKPI(TestCase):
+@pytest.fixture
+def savings_kpi():
+    return SavingsKPI()
 
-    def setUp(self) -> None:
-        self.savings_kpi = SavingsKPI()
 
-    def tearDown(self) -> None:
-        pass
-
-    def test_populate_consumer_producer_sets_are_correct(self):
-        self.pv_uuid = str(uuid4())
-        self.load_uuid = str(uuid4())
-        self.ess_uuid = str(uuid4())
-        test_area = {
-             "name": "house",
-             "children": [
-                 {"name": "pv1", "uuid": self.pv_uuid, "type": "PV", "panel_count": 4},
-                 {"name": "load1", "uuid": self.load_uuid, "type": "LoadHoursStrategy",
-                  "avg_power_W": 200},
-                 {"name": "storage1", "uuid": self.ess_uuid, "type": "StorageExternalStrategy"}
-             ]
-           }
-        self.savings_kpi.populate_consumer_producer_sets(test_area)
-        assert self.savings_kpi.producer_ess_set == {self.pv_uuid, self.ess_uuid}
-        assert self.savings_kpi.consumer_ess_set == {self.load_uuid, self.ess_uuid}
+def test_populate_consumer_producer_sets_are_correct(savings_kpi):
+    pv_uuid = str(uuid4())
+    load_uuid = str(uuid4())
+    ess_uuid = str(uuid4())
+    test_area = {
+         "name": "house",
+         "children": [
+             {"name": "pv1", "uuid": pv_uuid, "type": "PV", "panel_count": 4},
+             {"name": "load1", "uuid": load_uuid, "type": "LoadHoursStrategy",
+              "avg_power_W": 200},
+             {"name": "storage1", "uuid": ess_uuid, "type": "StorageExternalStrategy"}
+         ]
+       }
+    savings_kpi.populate_consumer_producer_sets(test_area)
+    assert savings_kpi.producer_ess_set == {pv_uuid, ess_uuid}
+    assert savings_kpi.consumer_ess_set == {load_uuid, ess_uuid}
