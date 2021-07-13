f6b6464064b6f8278e717b7d677c2e695d2d0d58
faizan2590 <faizan2590@gmail.com>
2021-07-13 20:11:35 +0200

    D3ASIM-3464: Adding test for root to target are grid fee accumulation.

--- tests/test_savings_kpi.py
+++ tests/test_savings_kpi.py
@@ -1,12 +1,17 @@
 import pytest
 from uuid import uuid4
 
-from d3a_interface.sim_results.kpi import SavingsKPI
+from d3a_interface.sim_results.kpi import KPI, SavingsKPI
 
 
 @pytest.fixture
 def savings_kpi():
     return SavingsKPI()
+
+
+@pytest.fixture
+def kpi():
+    return KPI()
 
 
 def test_populate_consumer_producer_sets_are_correct(savings_kpi):
@@ -25,3 +30,36 @@ def test_populate_consumer_producer_sets_are_correct(savings_kpi):
     savings_kpi.populate_consumer_producer_sets(test_area)
     assert savings_kpi.producer_ess_set == {pv_uuid, ess_uuid}
     assert savings_kpi.consumer_ess_set == {load_uuid, ess_uuid}
+
+
+def test_root_to_target_area_grid_fee_accumulation(kpi):
+    grid_uuid = str(uuid4())
+    house1_uuid = str(uuid4())
+    house2_uuid = str(uuid4())
+    pv_uuid = str(uuid4())
+    load_uuid = str(uuid4())
+    pv2_uuid = str(uuid4())
+    area_dict = {
+        "name": "grid", "uuid": grid_uuid, "parent_uuid": "", "children": [
+            {"name": "house1", "type": "Area", "uuid": house1_uuid, "parent_uuid": grid_uuid,
+             "children": [
+                 {"name": "pv", "type": "PVStrategy", "uuid": pv_uuid, "parent_uuid": house1_uuid,
+                  "children": []},
+                 {"name": "load", "type": "LoadHoursStrategy", "uuid": load_uuid,
+                  "parent_uuid": house1_uuid, "children": []}
+             ]},
+            {"name": "house2", "type": "Area", "uuid": house2_uuid, "parent_uuid": grid_uuid,
+             "children": [
+                 {"name": "pv2", "type": "PVStrategy", "uuid": pv2_uuid,
+                  "parent_uuid": house2_uuid, "children": []},
+             ]}
+        ]}
+    current_market_slot = "2021-09-13T12:45"
+    core_stats = {
+        grid_uuid: {"const_fee_rate": 1},
+        house1_uuid: {"const_fee_rate": 0.5}
+    }
+    kpi.update(area_dict, core_stats, current_market_slot)
+    assert kpi.area_uuid_cum_grid_fee_mapping[grid_uuid] == 1.0
+    assert kpi.area_uuid_cum_grid_fee_mapping[house1_uuid] == 1.5
+    assert kpi.area_uuid_cum_grid_fee_mapping[house2_uuid] == 1.0
