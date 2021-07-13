36631844f6795714034b65d5fda3c9f9714542e2
Hassan Hammoud <hassan.hammoud754@gmail.com>
2021-07-13 14:58:58 +0300

    D3ASIM-3356: Update Base Algorithm type hint

--- d3a_interface/matching_algorithms/abstract_matching_algorithm.py
+++ d3a_interface/matching_algorithms/abstract_matching_algorithm.py
@@ -1,13 +1,16 @@
 from abc import ABC, abstractmethod
 from typing import Dict, List
 
+from d3a_interface.dataclasses import BidOfferMatch
+
 
 class BaseMatchingAlgorithm(ABC):
 
     @classmethod
     @abstractmethod
-    def get_matches_recommendations(cls, matching_data: Dict) -> List:
-        """Calculate and return matches recommendations
+    def get_matches_recommendations(
+            cls, matching_data: Dict) -> List[BidOfferMatch.serializable_dict]:
+        """Calculate and return matches recommendations.
 
         Args:
             matching_data: {market_uuid: {"offers": [...], "bids": [...], "current_time":"",...}}
