2af90b51c92785cbfd1678d0a6d7920c01b804b7
Hassan Hammoud <hassan.hammoud754@gmail.com>
2021-07-19 17:26:37 +0300

    D3ASIM-3356: Fix pay as bid unit test

--- tests/test_matching_algorithms/test_pay_as_bid_matching_algorithm.py
+++ tests/test_matching_algorithms/test_pay_as_bid_matching_algorithm.py
@@ -33,23 +33,19 @@ class TestPayAsBidMatchingAlgorithm:
             }
         }
         trades = PayAsBidMatchingAlgorithm.get_matches_recommendations(data)
-        expected_trades = [{"market_id": "market1",
-                            "bid": {"id": 3, "buyer": "C", "energy_rate": 3, "energy": 20},
-                            "offer": {"id": 4, "seller": "A", "energy_rate": 1.00001,
-                                      "energy": 25}, "selected_energy": 20, "trade_rate": 3},
+        expected_trades = [
+            {"market_id": "market1",
+             "bid": {"id": 3, "buyer": "C", "energy_rate": 3, "energy": 20},
+             "offer": {"id": 4, "seller": "A", "energy_rate": 1.00001, "energy": 25},
+             "selected_energy": 20, "trade_rate": 3},
 
-                           {"market_id": "market2",
-                            "bid": {"id": 9, "buyer": "C", "energy_rate": 6, "energy": 50},
-                            "offer": {"id": 10, "seller": "A", "energy_rate": 1, "energy": 55},
-                            "selected_energy": 50, "trade_rate": 6},
+            {"market_id": "market2",
+             "bid": {"id": 8, "buyer": "B", "energy_rate": 2, "energy": 45},
+             "offer": {"id": 12, "seller": "C", "energy_rate": 1, "energy": 65},
+             "selected_energy": 45, "trade_rate": 2},
 
-                           {"market_id": "market2",
-                            "bid": {"id": 7, "buyer": "A", "energy_rate": 1.5, "energy": 40},
-                            "offer": {"id": 11, "seller": "B", "energy_rate": 1, "energy": 60},
-                            "selected_energy": 40, "trade_rate": 1.5},
-
-                           {"market_id": "market2",
-                            "bid": {"id": 8, "buyer": "B", "energy_rate": 2, "energy": 45},
-                            "offer": {"id": 12, "seller": "C", "energy_rate": 1, "energy": 65},
-                            "selected_energy": 45, "trade_rate": 2}]
+            {"market_id": "market2",
+             "bid": {"id": 9, "buyer": "C", "energy_rate": 6, "energy": 50},
+             "offer": {"id": 11, "seller": "B", "energy_rate": 1, "energy": 60},
+             "selected_energy": 50, "trade_rate": 6}]
         assert trades == expected_trades
