8347c7ee2628ad1a639b45d1b89c534b0033a60a
Pierpaolo Pantone <24alsecondo@gmail.com>
2021-11-10 12:15:28 +0100

    Use __all__ to define imported modules in init
    This also fixes flake8 complaints

--- d3a_interface/__init__.py
+++ d3a_interface/__init__.py
@@ -1,5 +1,7 @@
 import sys
 
-import gsy_framework  # NOQA
+import gsy_framework
 
-sys.modules[__name__] = sys.modules['gsy_framework']
+__all__ = ["gsy_framework"]
+
+sys.modules[__name__] = sys.modules["gsy_framework"]
