dc8a5525aeb4a21448a1ae81e9b55a0db5db9d72
Pierpaolo Pantone <24alsecondo@gmail.com>
2021-11-10 12:17:43 +0100

    Show warning when importing deprecated d3a_interface

--- d3a_interface/__init__.py
+++ d3a_interface/__init__.py
@@ -1,6 +1,15 @@
+import logging
 import sys
 
 import gsy_framework
+
+logger = logging.getLogger(__name__)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(logging.Formatter("%(levelname)s: %(message)s"))
+logger.addHandler(console_handler)
+
+logger.warning(
+    "The d3a_interface module name will be deprecated soon. Please use gsy_framework instead.")
 
 __all__ = ["gsy_framework"]
 
