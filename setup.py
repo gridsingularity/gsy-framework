560c216d19c378ebea586e2f7bdfd8912265a09b
Pierpaolo Pantone <24alsecondo@gmail.com>
2021-12-20 15:59:47 +0100

    Bump version: 1.1.0 → 1.2.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r", encoding="utf-8") as readme:
     README = readme.read()
 
 # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-VERSION = "1.1.0"
+VERSION = "1.2.0"
 
 setup(
     name="gsy-framework",
