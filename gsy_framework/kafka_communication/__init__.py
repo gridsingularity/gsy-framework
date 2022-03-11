925af5505117ca97bd69265f25f4786cf6b8fc3b
spyrostz <spyros@gridsingularity.com>
2022-03-11 09:14:20 +0100

    Removed DEFAULT_KAFKA_URL in favor of maintaining only KAFKA_URL variable. Created variable IS_KAFKA_RUNNING_LOCALLY in order to explicitly decide whether Kafka authentication should be used or not.

--- gsy_framework/kafka_communication/__init__.py
+++ gsy_framework/kafka_communication/__init__.py
@@ -1,21 +1,22 @@
 import ssl
 from os import environ
 
-DEFAULT_KAFKA_URL = environ.get("DEFAULT_KAFKA_URL", "localhost:9092")
-KAFKA_URL = environ.get("KAFKA_URL", DEFAULT_KAFKA_URL)
+
+IS_KAFKA_RUNNING_LOCALLY = environ.get("IS_KAFKA_RUNNING_LOCALLY", "false") == "false"
+KAFKA_URL = environ.get("KAFKA_URL", "localhost:9092")
 KAFKA_USERNAME = environ.get("KAFKA_USERNAME", None)
 KAFKA_PASSWORD = environ.get("KAFKA_PASSWORD", None)
-KAFKA_COMMUNICATION_SECURITY_PROTOCOL = \
-    environ.get("KAFKA_COMMUNICATION_SECURITY_PROTOCOL", "SASL_SSL")
-KAFKA_SASL_AUTH_MECHANISM = \
-    environ.get("KAFKA_SASL_AUTH_MECHANISM", "SCRAM-SHA-512")
+KAFKA_COMMUNICATION_SECURITY_PROTOCOL = (
+    environ.get("KAFKA_COMMUNICATION_SECURITY_PROTOCOL", "SASL_SSL"))
+KAFKA_SASL_AUTH_MECHANISM = (
+    environ.get("KAFKA_SASL_AUTH_MECHANISM", "SCRAM-SHA-512"))
 KAFKA_API_VERSION = (0, 10)
 KAFKA_RESULTS_TOPIC = environ.get("KAFKA_RESULTS_TOPIC", "d3a-results")
 KAFKA_CONSUMER_GROUP_ID = environ.get("KAFKA_RESULTS_GROUP_ID", "d3a-results-group")
 
 
 def create_kafka_new_ssl_context():
-    # Create a new context using system defaults, disable all but TLS1.2
+    """Create a new context using system defaults, disable all but TLS1.2."""
     ssl_context = ssl.create_default_context()
     ssl_context.options &= ssl.OP_NO_TLSv1
     ssl_context.options &= ssl.OP_NO_TLSv1_1
