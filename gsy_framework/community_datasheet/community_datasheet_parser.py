ab1dfb51bcb5157ea990f0a0d9b286c268a38393
Pierpaolo Pantone <24alsecondo@gmail.com>
2022-06-10 14:30:20 +0200

    GSYE-261: Separate datasheet reader functionality into a separate class

--- gsy_framework/community_datasheet/community_datasheet_parser.py
+++ gsy_framework/community_datasheet/community_datasheet_parser.py
@@ -1,6 +1,5 @@
 """Module for parsing SCM Community Datasheet Excel files."""
 
-import json
 import logging
 import uuid
 from itertools import chain
@@ -8,15 +7,12 @@ from typing import Dict
 
 import requests
 from jsonschema.exceptions import ValidationError
-from openpyxl import load_workbook
-from openpyxl.utils.exceptions import InvalidFileException
 
+from gsy_framework.community_datasheet.community_datasheet_reader import (
+    CommunityDatasheet, CommunityDatasheetReader)
 from gsy_framework.community_datasheet.exceptions import CommunityDatasheetException
 from gsy_framework.community_datasheet.location_converter import (
     LocationConverter, LocationConverterException)
-from gsy_framework.community_datasheet.sheet_parsers import (
-    CommunityMembersSheetParser, GeneralSettingsSheetParser, LoadSheetParser, ProfileSheetParser,
-    PVSheetParser, StorageSheetParser)
 from gsy_framework.scenario_validators import scenario_validator
 
 logger = logging.getLogger(__name__)
@@ -29,67 +25,24 @@ PROFILE_KEYS_BY_TYPE = {
 
 
 class CommunityDatasheetParser:
-    """Parser for Community Datasheet files.
+    """Parser for the community datasheet."""
+    def __init__(self, datasheet: CommunityDatasheet):
+        self._datasheet = datasheet
 
-    Args:
-        filename: the path to open or a file-like object.
-    """
-    SHEETNAMES = {"General settings", "Community Members", "Load", "PV", "Storage", "Profiles"}
+    def parse(self):
+        """Read the raw datasheet and convert it to grid-like items."""
 
-    def __init__(self, filename):
-        self._filename = filename
-
-        self.settings: Dict = None
-        self.members: Dict = None
-        self.loads: Dict = None
-        self.pvs: Dict = None
-        self.storages: Dict = None
-        self.profiles: Dict = None
-
-    def parse(self) -> Dict:
-        """Parse the entire datasheet and return its representation as a dictionary."""
-        workbook = self._load_workbook(self._filename)
-
-        self._validate_sheetnames(workbook)
-        self._parse_sheets(workbook)
-
-        self._fetch_pv_coordinates(self.pvs, self.members)
-
-        self._validate_missing_profiles()
-
+        self._fetch_pv_coordinates(self._datasheet.pvs, self._datasheet.members)
         assets_by_member = self._get_assets_by_member()
 
-        self._merge_profiles_into_assets(self.profiles, assets_by_member)
+        self._merge_profiles_into_assets(self._datasheet.profiles, assets_by_member)
         grid = self._create_grid(assets_by_member)
 
         self._validate_grid(grid)
-
         return {
-            "settings": self.settings,
+            "settings": self._datasheet.settings,
             "grid": grid
         }
-
-    def as_json(self) -> str:
-        """Return the JSON representation of the datasheet."""
-        return json.dumps(self.parse(), indent=4)
-
-    @staticmethod
-    def _load_workbook(filename: str):
-        try:
-            return load_workbook(filename, read_only=True)
-        except InvalidFileException as ex:
-            logger.debug("Error while parsing community datasheet: %s", ex)
-            raise CommunityDatasheetException(
-                "Community Datasheet format not supported. "
-                "Please make sure to use a proper Excel .xlsx file.") from ex
-
-    @classmethod
-    def _validate_sheetnames(cls, workbook) -> None:
-        current_sheetnames = set(workbook.sheetnames)
-        if not current_sheetnames == cls.SHEETNAMES:
-            raise CommunityDatasheetException(
-                f"The datasheet should contain the following sheets: {cls.SHEETNAMES}."
-                f"Found: {current_sheetnames}.")
 
     @staticmethod
     def _validate_grid(grid):
@@ -98,39 +51,17 @@ class CommunityDatasheetParser:
         except ValidationError as ex:
             raise CommunityDatasheetException(ex) from ex
 
-    def _validate_missing_profiles(self):
-        asset_names = []
-        # Storage assets do not define profiles so we don't consider them
-        asset_items = [assets.values() for assets in [self.loads, self.pvs]]
-        for assets in chain.from_iterable(asset_items):
-            asset_names.extend(asset["name"] for asset in assets)
-
-        missing_profiles = [
-            asset_name for asset_name in asset_names
-            if asset_name not in self.profiles
-        ]
-
-        if missing_profiles:
-            raise CommunityDatasheetException(
-                "Each asset must explicitly define an energy profile in the datasheet. "
-                f"The following assets do not define a profile: {missing_profiles}.")
-
-    def _parse_sheets(self, workbook) -> None:
-        self.settings = GeneralSettingsSheetParser(workbook["General settings"]).parse()
-        self.members = CommunityMembersSheetParser(workbook["Community Members"]).parse()
-        self.loads = LoadSheetParser(workbook["Load"]).parse()
-        self.pvs = PVSheetParser(workbook["PV"]).parse()
-        self.storages = StorageSheetParser(workbook["Storage"]).parse()
-        self.profiles = ProfileSheetParser(workbook["Profiles"]).parse()
-
     def _get_assets_by_member(self) -> Dict:
         """Return a mapping between each member and their assets."""
-        assets_by_member = {member_name: [] for member_name in self.members}
-        asset_items = [assets.items() for assets in [self.loads, self.pvs, self.storages]]
+        assets_by_member = {member_name: [] for member_name in self._datasheet.members}
+        asset_items = [
+            assets.items()
+            for assets in [self._datasheet.loads, self._datasheet.pvs, self._datasheet.storages]
+        ]
         for member_name, assets in chain.from_iterable(asset_items):
             assets_by_member[member_name].extend(assets)
 
-        if any((missing := member) not in self.members for member in assets_by_member):
+        if any((missing := member) not in self._datasheet.members for member in assets_by_member):
             raise CommunityDatasheetException(
                 f'Member "{missing}" was defined in one of the asset sheets'  # noqa: F821
                 'but not in the "Community Members" sheet.')
@@ -169,7 +100,7 @@ class CommunityDatasheetParser:
 
     def _create_member_representation(self, member_name: str) -> Dict:
         return {
-            **self.members[member_name],
+            **self._datasheet.members[member_name],
             **{
                 "name": member_name,
                 "tags": ["Home"],
@@ -212,9 +143,9 @@ class CommunityDatasheetParser:
 
 def parse_community_datasheet(filename):
     """Parse the content of the community datasheet and return a JSON representation."""
-    datasheet = CommunityDatasheetParser(filename)
-    output = datasheet.parse()
-    print(json.dumps(output, indent=4))
+    datasheet = CommunityDatasheetReader.read(filename)
+    parser = CommunityDatasheetParser(datasheet)
+    print(parser.parse())
 
 
 if __name__ == "__main__":
---
+++ gsy_framework/community_datasheet/community_datasheet_reader.py
@@ -0,0 +1,100 @@
+"""Module for the reader of the SCM community datasheet."""
+
+import json
+import logging
+from dataclasses import dataclass
+from itertools import chain
+from typing import Dict
+
+from openpyxl import load_workbook
+from openpyxl.utils.exceptions import InvalidFileException
+
+from gsy_framework.community_datasheet.exceptions import CommunityDatasheetException
+from gsy_framework.community_datasheet.sheet_parsers import (
+    CommunityMembersSheetParser, GeneralSettingsSheetParser, LoadSheetParser, ProfileSheetParser,
+    PVSheetParser, StorageSheetParser)
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class CommunityDatasheet:
+    """Dataclass for the parsed contents of the Community Datasheet."""
+
+    settings: Dict
+    members: Dict
+    loads: Dict
+    pvs: Dict
+    storages: Dict
+    profiles: Dict
+
+    def as_json(self) -> str:
+        """Return the JSON representation of the datasheet."""
+        return json.dumps(self, indent=4)
+
+
+class CommunityDatasheetReader:
+    """Parser for Community Datasheet files.
+
+    Args:
+        filename: the path to open or a file-like object.
+    """
+    SHEETNAMES = {"General settings", "Community Members", "Load", "PV", "Storage", "Profiles"}
+
+    @classmethod
+    def read(cls, filename) -> Dict:
+        """Parse the entire datasheet and return its representation as a dictionary."""
+        workbook = cls._load_workbook(filename)
+
+        cls._validate_sheetnames(workbook)
+        datasheet = cls._parse_sheets(workbook)
+        cls._validate_missing_profiles(datasheet)
+
+        return datasheet
+
+    @staticmethod
+    def _load_workbook(filename: str):
+        try:
+            return load_workbook(filename, read_only=True)
+        except InvalidFileException as ex:
+            logger.debug("Error while parsing community datasheet: %s", ex)
+            raise CommunityDatasheetException(
+                "Community Datasheet format not supported. "
+                "Please make sure to use a proper Excel .xlsx file.") from ex
+
+    @classmethod
+    def _validate_sheetnames(cls, workbook) -> None:
+        current_sheetnames = set(workbook.sheetnames)
+        if not current_sheetnames == cls.SHEETNAMES:
+            raise CommunityDatasheetException(
+                f"The datasheet should contain the following sheets: {cls.SHEETNAMES}."
+                f"Found: {current_sheetnames}.")
+
+    @staticmethod
+    def _parse_sheets(workbook) -> CommunityDatasheet:
+        return CommunityDatasheet(
+            settings=GeneralSettingsSheetParser(workbook["General settings"]).parse(),
+            members=CommunityMembersSheetParser(workbook["Community Members"]).parse(),
+            loads=LoadSheetParser(workbook["Load"]).parse(),
+            pvs=PVSheetParser(workbook["PV"]).parse(),
+            storages=StorageSheetParser(workbook["Storage"]).parse(),
+            profiles=ProfileSheetParser(workbook["Profiles"]).parse()
+        )
+
+    @staticmethod
+    def _validate_missing_profiles(datasheet):
+        asset_names = []
+        # Storage assets do not define profiles so we don't consider them
+        asset_items = [assets.values() for assets in [datasheet.loads, datasheet.pvs]]
+        for assets in chain.from_iterable(asset_items):
+            asset_names.extend(asset["name"] for asset in assets)
+
+        missing_profiles = [
+            asset_name for asset_name in asset_names
+            if asset_name not in datasheet.profiles
+        ]
+
+        if missing_profiles:
+            raise CommunityDatasheetException(
+                "Each asset must explicitly define an energy profile in the datasheet. "
+                f"The following assets do not define a profile: {missing_profiles}.")
