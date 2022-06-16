8c2aa26456212b78151a982b4aba386bbe651152
Spyros Tzavikas <spyros@gridsingularity.com>
2022-06-16 14:48:27 +0200

    GSYE-256: Added assertion on the Trade object constructor, in order to avoid cases where the traded energy is negative or zero. Handled cornercase where the offer_bid member of the Trade class is None, which would lead to an AttributeError while logging.

--- gsy_framework/data_classes.py
+++ gsy_framework/data_classes.py
@@ -29,7 +29,7 @@ from typing import List, Dict, Optional, Tuple, Union
 
 from pendulum import DateTime
 
-from gsy_framework.constants_limits import DEFAULT_PRECISION
+from gsy_framework.constants_limits import DEFAULT_PRECISION, FLOATING_POINT_TOLERANCE
 from gsy_framework.utils import (
     limit_float_precision, datetime_to_string_incl_seconds, key_in_dict_and_not_none,
     str_to_pendulum_datetime)
@@ -342,6 +342,9 @@ class Trade:
         self.offer_bid = offer_bid
         self.seller = seller
         self.buyer = buyer
+
+        assert (traded_energy > FLOATING_POINT_TOLERANCE,
+                f"Traded energy should always be a positive nonzero value.")
         self.traded_energy = traded_energy
         self.trade_price = trade_price
         self.residual = residual
@@ -361,7 +364,7 @@ class Trade:
             f"{{{self.id!s:.6s}}} [origin: {self.seller_origin} -> {self.buyer_origin}] "
             f"[{self.seller} -> {self.buyer}] {self.traded_energy} kWh @ {self.trade_price}"
             f" {round(self.trade_rate, 8)} "
-            f"{self.offer_bid.id} [fee: {self.fee_price} cts.] "
+            f"{self.offer_bid.id if self.offer_bid else ''} [fee: {self.fee_price} cts.] "
             f"{self.matching_requirements or ''}")
 
     @classmethod
