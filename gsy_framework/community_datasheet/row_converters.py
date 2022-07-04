c278681395c5d9fb6c29d6062d4e037d2b6e7ac2
Pierpaolo Pantone <24alsecondo@gmail.com>
2022-07-04 10:47:17 +0200

    GSYE-261: Add validation for required datasheet fields

--- gsy_framework/community_datasheet/row_converters.py
+++ gsy_framework/community_datasheet/row_converters.py
@@ -1,7 +1,7 @@
 import re
 import uuid
 from datetime import datetime, timedelta
-from typing import Dict
+from typing import Dict, Tuple
 
 import pendulum
 
@@ -10,6 +10,8 @@ from gsy_framework.community_datasheet.exceptions import (
 from gsy_framework.community_datasheet.sheet_headers import (
     LoadSheetHeader, PVSheetHeader, StorageSheetHeader)
 from gsy_framework.constants_limits import ConstSettings
+
+NULL_VALUES = (None, "")
 
 
 class StringToTimedeltaParser:
@@ -47,6 +49,8 @@ class GeneralSettingsRowConverter:
 
         The field name is converted to snake case.
         """
+        cls._validate_row(row)
+
         field_name, value = row[0], row[2]  # Each row is a tuple (setting-name, legend, value)
         field_name = field_name.strip().lower().replace(" ", "_")
         if field_name in ("start_date", "end_date") and isinstance(value, datetime):
@@ -60,13 +64,25 @@ class GeneralSettingsRowConverter:
 
         return {field_name.strip().lower(): value}
 
+    @classmethod
+    def _validate_row(cls, row: Tuple):
+        if row[2] in NULL_VALUES:
+            raise CommunityDatasheetException((
+                "Missing value for required field. "
+                f"Row: {row}. Required field: {row[0]}."))
+
 
 class MembersRowConverter:
     """Convert from the excel row to a dictionary representing member information."""
 
+    OPTIONAL_FIELDS = ("Location/Address (optional)",)
+
     @classmethod
     def convert(cls, row: Dict) -> Dict:
         """Convert the row using keys that will be added to the grid representation."""
+
+        cls._validate_row(row)
+
         return {
             "email": row["Email"],
             "zip_code": row["ZIP code"],
@@ -80,6 +96,17 @@ class MembersRowConverter:
             "coefficient_percentage": row["Coefficient"]
         }
 
+    @classmethod
+    def _validate_row(cls, row: Dict):
+        missing_fields = [
+            field for field, value in row.items()
+            if field not in cls.OPTIONAL_FIELDS and value in NULL_VALUES]
+
+        if missing_fields:
+            raise CommunityDatasheetException((
+                "Missing values for required fields. "
+                f"Row: {row}. Required fields: {missing_fields}."))
+
 
 class LoadRowConverter:
     """Convert from the excel row to a grid representation of a Load asset."""
@@ -89,21 +116,37 @@ class LoadRowConverter:
         """
         Convert the row parsed from the Excel file to the asset representation used in scenarios.
         """
+        cls._validate_row(row)
+
         return {
             "name": row[LoadSheetHeader.LOAD_NAME],
             "type": "Load",
             "uuid": str(uuid.uuid4())
         }
 
+    @classmethod
+    def _validate_row(cls, row: Dict):
+        missing_fields = [field for field, value in row.items() if value in NULL_VALUES]
+
+        if missing_fields:
+            raise CommunityDatasheetException((
+                "Missing values for required fields. "
+                f"Row: {row}. Required fields: {missing_fields}."))
+
 
 class PVRowConverter:
     """Convert from the excel row to a grid representation of a PV asset."""
+
+    # These fields are optional because they can be ignored if a PV profile is explicitly provided
+    OPTIONAL_FIELDS = ("Capacity [kW]", "Tilt [°]", "Azimuth [°]")
 
     @classmethod
     def convert(cls, row: Dict) -> Dict:
         """
         Convert the row parsed from the Excel file to the asset representation used in scenarios.
         """
+        cls._validate_row(row)
+
         return {
             "name": row[PVSheetHeader.PV_NAME],
             "type": "PV",
@@ -114,6 +157,17 @@ class PVRowConverter:
             "azimuth": row[PVSheetHeader.AZIMUTH]
         }
 
+    @classmethod
+    def _validate_row(cls, row: Dict):
+        missing_fields = [
+            field for field, value in row.items()
+            if field not in cls.OPTIONAL_FIELDS and value in NULL_VALUES]
+
+        if missing_fields:
+            raise CommunityDatasheetException((
+                "Missing values for required fields. "
+                f"Row: {row}. Required fields: {missing_fields}."))
+
 
 class StorageRowConverter:
     """Convert from the excel row to a grid representation of a Storage asset."""
@@ -123,6 +177,8 @@ class StorageRowConverter:
         """
         Convert the row parsed from the Excel file to the asset representation used in scenarios.
         """
+        cls._validate_row(row)
+
         return {
             "name": row[StorageSheetHeader.BATTERY_NAME],
             "type": "Storage",
@@ -137,3 +193,12 @@ class StorageRowConverter:
                 row[StorageSheetHeader.MAXIMUM_POWER_KW]
                 or ConstSettings.StorageSettings.MAX_ABS_POWER)
         }
+
+    @classmethod
+    def _validate_row(cls, row: Dict):
+        missing_fields = [field for field, value in row.items() if value in NULL_VALUES]
+
+        if missing_fields:
+            raise CommunityDatasheetException((
+                "Missing values for required fields. "
+                f"Row: {row}. Required fields: {missing_fields}."))
