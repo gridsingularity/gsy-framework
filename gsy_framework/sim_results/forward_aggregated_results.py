4a7abbded5ed9b54fd70f5bce9febef0489f1eed
Amirhossein Saemi <amirhossein@gridsingularity.com>
2022-08-25 08:55:00 +0200

    GSYE-375: Enhance code readability

--- gsy_framework/sim_results/forward_aggregated_results.py
+++ gsy_framework/sim_results/forward_aggregated_results.py
@@ -4,12 +4,17 @@ from pendulum import DateTime, Duration
 
 
 class MarketResultsAggregator:
-    """Calculate aggregated market results in different resolutions."""
+    """Calculate aggregated/accumulated market results in different resolutions."""
 
-    def __init__(self, last_aggregated_result=None):
+    def __init__(self, resolution: Duration, last_aggregated_result: Dict = None,
+                 aggregators: Dict[str, Callable] = None,
+                 accumulators: Dict[str, Callable] = None):
         self.bids_offers_trades: Dict[DateTime, Dict] = {}
         self.last_aggregated_result = last_aggregated_result if \
             last_aggregated_result is not None else {}
+        self.resolution = resolution
+        self.aggregators = aggregators if aggregators else {}
+        self.accumulators = accumulators if accumulators else {}
 
     def update(self, current_timeslot: DateTime, market_stats) -> None:
         """Update the buffer of bids_offers_trades with the result
@@ -26,13 +31,10 @@ class MarketResultsAggregator:
             for order in self.bids_offers_trades[current_timeslot]:
                 self.bids_offers_trades[current_timeslot][order].extend(market_data[order])
 
-    def generate(self, resolution: Duration,
-                 aggregators: Dict[str, Callable] = None,
-                 accumulators: Dict[str, Callable] = None):
-        """Generate aggregated results in the specified resolution from the raw results.
-        If no resolution is given, then the data in its original resolution will be aggregated.
-        If the resolution is smaller than the timedelta between consequent, then the same data
-        will be returned.
+    def generate(self):
+        """Generate aggregated/accumulated results in the specified resolution from the
+        raw results. If the given resolution is smaller than the timedelta between consequent
+        market slots, then the same data will be returned.
         """
         if not self.bids_offers_trades:
             return
@@ -40,17 +42,16 @@ class MarketResultsAggregator:
         # todo: add assertions to check no timeslot is missing with regards to the resolution
         sorted_market_timeslots = sorted(self.bids_offers_trades)
         start_time = sorted_market_timeslots[0]
-        next_time = start_time + resolution
+        next_time = start_time + self.resolution
         timeslots_to_aggregate = []
         for timeslot in sorted_market_timeslots:
             if timeslot >= next_time:
                 if timeslots_to_aggregate:
-                    yield self._aggregated_results(timeslots_to_aggregate,
-                                                   aggregators, accumulators)
+                    yield self._prepare_results(timeslots_to_aggregate)
                     self._remove_processed_timeslots(timeslots_to_aggregate)
                     timeslots_to_aggregate = []
                 timeslots_to_aggregate.append(timeslot)
-                next_time += resolution
+                next_time += self.resolution
             else:
                 timeslots_to_aggregate.append(timeslot)
 
@@ -59,43 +60,44 @@ class MarketResultsAggregator:
         for timeslot in timeslots:
             del self.bids_offers_trades[timeslot]
 
-    def _aggregated_results(self, timeslots: List[DateTime], aggregators, accumulators):
-        """Call all the aggregation function on the grouped timeslots data."""
-        if aggregators is None:
-            aggregators = {}
-
-        # todo: skip this for more performance!
-        collected_data = {"bids": [], "offers": [], "trades": []}
+    def _prepare_results(self, timeslots: List[DateTime]):
+        """Call all the aggregation/accumulation functions on the grouped timeslots data."""
+        # This part can be compromised for more performance.
+        # An accumulator/aggregator could be also passed the timeslots lists
+        # and the bids_offers_trades buffer to process the current results.
+        collected_raw_data = {"bids": [], "offers": [], "trades": []}
         for timeslot in timeslots:
-            for order, collected_order in collected_data.items():
+            for order, collected_order in collected_raw_data.items():
                 collected_order.extend(self.bids_offers_trades[timeslot][order])
 
-        aggregated_results = {}
-        for aggregator_name, aggregator_fn in aggregators.items():
-            aggregated_results[aggregator_name] = aggregator_fn(collected_data)
-
         result = {
-            "current_results": {
-                "start_time": timeslots[0],
-                **aggregated_results
-            },
-            "accumulated_results": self._accumulated_results(collected_data, accumulators)
+            "start_time": timeslots[0],
+            "aggregated_results": self._aggregated_results(collected_raw_data),
+            "accumulated_results": self._accumulated_results(collected_raw_data)
         }
         self.last_aggregated_result = result
         return result
 
-    def _accumulated_results(self, collected_data: Dict, accumulators) -> Dict:
-        if accumulators is None:
-            accumulators = {}
+    def _aggregated_results(self, collected_raw_data: Dict) -> Dict:
+        """Call all the aggregator functions to calculate results for the
+        selected time window e.g. resolution"""
+        aggregated_results = {}
+        for aggregator_name, aggregator_fn in self.aggregators.items():
+            aggregated_results[aggregator_name] = aggregator_fn(collected_raw_data)
+        return aggregated_results
 
+    def _accumulated_results(self, collected_raw_data: Dict) -> Dict:
+        """Call all the accumulator functions to update global statistics of the whole market.
+        Accumulated results are results calculated from the beginning of the simulation.
+        """
         # todo: check if the last_aggregated_result is the correct last_aggregated_result
         last_accumulated_results = self.last_aggregated_result.get(
             "accumulated_results", {})
 
         accumulated_results = {}
-        for accumulator_name, accumulator in accumulators.items():
+        for accumulator_name, accumulator in self.accumulators.items():
             accumulated_results[accumulator_name] = accumulator(
-                last_accumulated_results.get(accumulator_name), collected_data)
+                last_accumulated_results.get(accumulator_name), collected_raw_data)
         return accumulated_results
 
 
