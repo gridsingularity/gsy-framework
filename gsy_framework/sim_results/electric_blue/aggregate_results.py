62c9650180c5371f83da8b2dc89f58e6bc19ed92
Amirhossein Saemi <amirhossein@gridsingularity.com>
2022-09-12 11:30:31 +0200

    GSYE-375: Add ForwardDeviceTimeSeries class

--- gsy_framework/sim_results/electric_blue/aggregate_results.py
+++ gsy_framework/sim_results/electric_blue/aggregate_results.py
@@ -1,6 +1,8 @@
 from collections import defaultdict
-from dataclasses import dataclass, fields
+from dataclasses import dataclass, field, fields
 from typing import Dict, List
+
+from pendulum import DateTime
 
 from gsy_framework.enums import AggregationResolution, AvailableMarketTypes
 
@@ -18,26 +20,42 @@ class ForwardDeviceStats:  # pylint: disable=too-many-instance-attributes
     """Hold forward device statistics for each timeslot. This class is used to first
     calculate the current results of device in the market and then used to merge it
     with the global results."""
+
     timeslot: str
     device_uuid: str
+    current_timeslot: DateTime
 
     # SELLER
     total_energy_produced: float = 0
     total_sell_trade_count: int = 0
     total_energy_sold: float = 0
     total_earned_eur: float = 0
+
     # BUYER
     total_energy_consumed: float = 0
     total_buy_trade_count: int = 0
     total_energy_bought: float = 0
     total_spent_eur: float = 0
 
+    open_offers: List[Dict] = field(default_factory=list)
+    open_bids: List[Dict] = field(default_factory=list)
+
     def __add__(self, other: "ForwardDeviceStats"):
         assert self.timeslot == other.timeslot
+
+        if self.current_timeslot >= other.current_timeslot:
+            current_timeslot = self.current_timeslot
+            open_bids = self.open_bids
+            open_offers = self.open_offers
+        else:
+            current_timeslot = other.current_timeslot
+            open_bids = other.open_bids
+            open_offers = other.open_offers
 
         return ForwardDeviceStats(
             timeslot=self.timeslot,
             device_uuid=self.device_uuid,
+            current_timeslot=current_timeslot,
             total_energy_produced=self.total_energy_produced + other.total_energy_produced,
             total_sell_trade_count=self.total_sell_trade_count + other.total_sell_trade_count,
             total_energy_sold=self.total_energy_sold + other.total_energy_sold,
@@ -45,12 +63,14 @@ class ForwardDeviceStats:  # pylint: disable=too-many-instance-attributes
             total_energy_consumed=self.total_energy_consumed + other.total_energy_consumed,
             total_buy_trade_count=self.total_buy_trade_count + other.total_buy_trade_count,
             total_energy_bought=self.total_energy_bought + other.total_energy_bought,
-            total_spent_eur=self.total_spent_eur + other.total_spent_eur
+            total_spent_eur=self.total_spent_eur + other.total_spent_eur,
+            open_bids=open_bids,
+            open_offers=open_offers
         )
 
     def to_dict(self) -> Dict:
         """Generate a dictionary for saving the data into DB."""
-        return {field.name: getattr(self, field.name) for field in fields(self)}
+        return {f.name: getattr(self, f.name) for f in fields(self)}
 
     def add_trade(self, trade: Dict) -> None:
         """Add trade information to device stats."""
@@ -63,9 +83,18 @@ class ForwardDeviceStats:  # pylint: disable=too-many-instance-attributes
             self.total_energy_bought += trade["energy"]
             self.total_spent_eur += trade["price"]
 
+    def add_bid(self, bid: Dict) -> None:
+        """Add bid information to device stats."""
+        self.open_bids.append(bid)
 
-def handle_forward_results(market_stats: Dict[str, Dict[str, List]]) -> Dict:
-    """Group trades by timeslot and device_uuid to accumulate trades for each device."""
+    def add_offer(self, offer: Dict) -> None:
+        """Add offer information to device stats."""
+        self.open_offers.append(offer)
+
+
+def handle_forward_results(
+        current_timeslot: DateTime, market_stats: Dict[str, Dict[str, List]]) -> Dict:
+    """Group trades by timeslot and device_uuid to accumulate orders for each device."""
     result = defaultdict(dict)
 
     if not market_stats:
@@ -78,13 +107,26 @@ def handle_forward_results(market_stats: Dict[str, Dict[str, List]]) -> Dict:
 
             if seller_id not in result[timeslot]:
                 result[timeslot][seller_id] = ForwardDeviceStats(
-                    timeslot=timeslot, device_uuid=seller_id)
+                    timeslot=timeslot, device_uuid=seller_id, current_timeslot=current_timeslot)
+            result[timeslot][seller_id].add_trade(trade)
 
             if buyer_id not in result[timeslot]:
                 result[timeslot][buyer_id] = ForwardDeviceStats(
-                    timeslot=timeslot, device_uuid=buyer_id)
-
-            result[timeslot][seller_id].add_trade(trade)
+                    timeslot=timeslot, device_uuid=buyer_id, current_timeslot=current_timeslot)
             result[timeslot][buyer_id].add_trade(trade)
+
+        for bid in timeslot_stats["bids"]:
+            buyer_id = bid["buyer_id"]
+            if buyer_id not in result[timeslot]:
+                result[timeslot][buyer_id] = ForwardDeviceStats(
+                    timeslot=timeslot, device_uuid=buyer_id, current_timeslot=current_timeslot)
+            result[timeslot][buyer_id].add_bid(bid)
+
+        for offer in timeslot_stats["offers"]:
+            seller_id = offer["seller_id"]
+            if seller_id not in result[timeslot]:
+                result[timeslot][seller_id] = ForwardDeviceStats(
+                    timeslot=timeslot, device_uuid=seller_id, current_timeslot=current_timeslot)
+            result[timeslot][seller_id].add_offer(offer)
 
     return result
---
+++ gsy_framework/sim_results/electric_blue/time_series.py
@@ -0,0 +1,133 @@
+from typing import Callable, Dict
+
+from gsy_e.models.strategy.energy_parameters.energy_params_eb import (
+    ForwardTradeProfileGenerator)
+from pendulum import DateTime, Duration
+
+from gsy_framework.enums import AvailableMarketTypes
+from gsy_framework.sim_results.electric_blue.aggregate_results import (
+    ForwardDeviceStats)
+
+
+def resample_data(
+        timeseries_data: Dict[DateTime, float],
+        resolution: Duration, aggregator_fn: Callable) -> Dict:
+    """Aggregate data using the specific resolution and aggregator function."""
+
+    result = {}
+
+    sorted_timeslots = sorted(timeseries_data.keys())
+    start_time = sorted_timeslots[0]
+    next_time = start_time + resolution
+
+    timeslots_to_aggregate = []
+    for timeslot in sorted_timeslots:
+        if timeslot >= next_time:
+            result[timeslots_to_aggregate[0]] = aggregator_fn(
+                [timeseries_data[t] for t in timeslots_to_aggregate])
+            timeslots_to_aggregate = [timeslot]
+            next_time += resolution
+        elif timeslot < next_time:
+            timeslots_to_aggregate.append(timeslot)
+
+    return result
+
+
+class ForwardDeviceTimeSeries:
+    """Generate time series data using ForwardDeviceStats object."""
+    def __init__(
+            self, device_stats: ForwardDeviceStats, peak_kWh: float,
+            product_type: AvailableMarketTypes):
+
+        self.device_stats = device_stats
+        self.product_type = product_type
+        self.trade_profile_generator = ForwardTradeProfileGenerator(peak_kWh=peak_kWh)
+
+    def _generate_matched_buy_orders_kWh(self, resolution: Duration) -> Dict:
+        """Generate time series for buy trades."""
+        if self.device_stats.total_energy_bought <= 0:
+            return {}
+
+        matched_buy_profile = self.trade_profile_generator.generate_trade_profile(
+            energy_kWh=self.device_stats.total_energy_bought,
+            market_slot=DateTime.fromisoformat(self.device_stats.timeslot),
+            product_type=self.product_type
+        )
+        return resample_data(matched_buy_profile, resolution, aggregator_fn=sum)
+
+    def _generate_matched_sell_orders_kWh(self, resolution: Duration) -> Dict:
+        """Generate time series for sell trades."""
+        if self.device_stats.total_energy_sold <= 0:
+            return {}
+
+        matched_sell_profile = self.trade_profile_generator.generate_trade_profile(
+            energy_kWh=self.device_stats.total_energy_sold,
+            market_slot=DateTime.fromisoformat(self.device_stats.timeslot),
+            product_type=self.product_type
+        )
+        return resample_data(matched_sell_profile, resolution, aggregator_fn=sum)
+
+    def _generate_open_sell_orders_kWh(self, resolution: Duration) -> Dict:
+        """Generate time series for all offers."""
+        if not self.device_stats.open_offers:
+            return {}
+        total_offered_energy = sum([offer["energy"] for offer in self.device_stats.open_offers])
+        open_sell_profile = self.trade_profile_generator.generate_trade_profile(
+            energy_kWh=total_offered_energy,
+            market_slot=DateTime.fromisoformat(self.device_stats.timeslot),
+            product_type=self.product_type
+        )
+        return resample_data(open_sell_profile, resolution, aggregator_fn=sum)
+
+    def _generate_open_buy_orders_kWh(self, resolution: Duration) -> Dict:
+        """Generate time series for all bids."""
+        if not self.device_stats.open_bids:
+            return {}
+        total_bade_energy = sum([bid["energy"] for bid in self.device_stats.open_bids])
+        open_buy_profile = self.trade_profile_generator.generate_trade_profile(
+            energy_kWh=total_bade_energy,
+            market_slot=DateTime.fromisoformat(self.device_stats.timeslot),
+            product_type=self.product_type
+        )
+        return resample_data(open_buy_profile, resolution, aggregator_fn=sum)
+
+    def _generate_all_buy_orders_kWh(self, resolution: Duration) -> Dict:
+        """Generate time series for all (bids + buy trades)."""
+        total_bade_energy = sum([bid["energy"] for bid in self.device_stats.open_bids])
+        all_buy_orders_kWh = self.device_stats.total_energy_bought + total_bade_energy
+
+        if all_buy_orders_kWh <= 0:
+            return {}
+
+        all_buy_profile = self.trade_profile_generator.generate_trade_profile(
+            energy_kWh=all_buy_orders_kWh,
+            market_slot=DateTime.fromisoformat(self.device_stats.timeslot),
+            product_type=self.product_type
+        )
+        return resample_data(all_buy_profile, resolution, aggregator_fn=sum)
+
+    def _generate_all_sell_orders(self, resolution):
+        """Generate time series for all (offers + sell trades)."""
+        total_offered_energy = sum([offer["energy"] for offer in self.device_stats.open_offers])
+        all_sell_orders_kWh = self.device_stats.total_energy_sold + total_offered_energy
+
+        if all_sell_orders_kWh <= 0:
+            return {}
+
+        all_sell_profile = self.trade_profile_generator.generate_trade_profile(
+            energy_kWh=all_sell_orders_kWh,
+            market_slot=DateTime.fromisoformat(self.device_stats.timeslot),
+            product_type=self.product_type
+        )
+        return resample_data(all_sell_profile, resolution, aggregator_fn=sum)
+
+    def generate(self, resolution: Duration):
+        """Generate all time series."""
+        return {
+            "matched_buy_orders_kWh": self._generate_matched_buy_orders_kWh(resolution),
+            "matched_sell_orders_kWh": self._generate_matched_sell_orders_kWh(resolution),
+            "open_sell_orders_kWh": self._generate_open_sell_orders_kWh(resolution),
+            "open_buy_orders_kWh": self._generate_matched_buy_orders_kWh(resolution),
+            "all_buy_orders_KWh": self._generate_all_buy_orders_kWh(resolution),
+            "all_sell_orders_kWh": self._generate_all_sell_orders(resolution)
+        }
