3796d20032e463253185535f799f13c4a4d9dc6b
Amirhossein Saemi <amirhossein@gridsingularity.com>
2022-09-23 15:19:35 +0200

    GSYE-392: Enhance performance

--- gsy_framework/sim_results/electric_blue/volume_timeseries.py
+++ gsy_framework/sim_results/electric_blue/volume_timeseries.py
@@ -5,10 +5,9 @@ from pendulum import DateTime
 from gsy_framework.enums import AggregationResolution, AvailableMarketTypes
 from gsy_framework.forward_markets.aggregated_profile import (
     get_aggregated_SSP_profile)
+from gsy_framework.forward_markets.forward_profile import ForwardTradeProfileGenerator
 from gsy_framework.sim_results.electric_blue.aggregate_results import (
     ForwardDeviceStats)
-from gsy_framework.sim_results.electric_blue.time_series import (
-    ForwardDeviceTimeSeries)
 
 FORWARD_MARKET_TYPES = [
     AvailableMarketTypes.YEAR_FORWARD, AvailableMarketTypes.MONTH_FORWARD,
@@ -40,20 +39,49 @@ class DeviceVolumeTimeSeries:
 
     def add(self, device_stats: ForwardDeviceStats, market_type: AvailableMarketTypes):
         """Add device time series to device volume time series."""
-        device_time_series = ForwardDeviceTimeSeries(device_stats, market_type).generate(
-            AggregationResolution.RES_15_MINUTES.duration())
 
-        for time_slot, value in device_time_series["matched_sell_orders_kWh"]:
-            self._add_to_volume_time_series(time_slot, value, f"{market_type}_sold")
+        self._add_total_energy_bought(device_stats, market_type)
+        self._add_total_energy_sold(device_stats, market_type)
 
-        for time_slot, value in device_time_series["matched_buy_orders_kWh"]:
-            self._add_to_volume_time_series(time_slot, value, f"{market_type}_bought")
+    def _add_total_energy_bought(
+            self, device_stats: ForwardDeviceStats, market_type: AvailableMarketTypes):
+        """Add statistics for the total amount of bought energy."""
+        energy_kWh = device_stats.total_energy_bought
+        if energy_kWh <= 0:
+            return
+        profile = ForwardTradeProfileGenerator(
+            peak_kWh=energy_kWh
+        ).generate_trade_profile(
+            energy_kWh=energy_kWh,
+            market_slot=device_stats.time_slot,
+            product_type=market_type
+        )
+        for time_slot, value in profile.items():
+            self._add_to_volume_time_series(time_slot, value, market_type, "bought_kWh")
 
-    def _add_to_volume_time_series(self, time_slot: DateTime, value: float, attribute_name: str):
+    def _add_total_energy_sold(
+            self, device_stats: ForwardDeviceStats, market_type: AvailableMarketTypes):
+        """Add statistics for the total amount of sold energy."""
+        energy_kWh = device_stats.total_energy_sold
+        if energy_kWh <= 0:
+            return
+        profile = ForwardTradeProfileGenerator(
+            peak_kWh=energy_kWh
+        ).generate_trade_profile(
+            energy_kWh=energy_kWh,
+            market_slot=device_stats.time_slot,
+            product_type=market_type
+        )
+        for time_slot, value in profile.items():
+            self._add_to_volume_time_series(time_slot, value, market_type, "sold_kWh")
+
+    def _add_to_volume_time_series(
+            self, time_slot: DateTime, value: float,
+            market_type: AvailableMarketTypes, attribute_name: str):
         """Add time slot value to the correct time slot to device volume time series."""
         time_slot = self._adapt_time_slot(time_slot)
         volume_time_series = self._get_device_volume_time_series(time_slot.start_of("year"))
-        volume_time_series[time_slot][attribute_name] += value
+        volume_time_series[time_slot][str(market_type)][attribute_name] += value
 
     def _adapt_time_slot(self, time_slot: DateTime) -> DateTime:
         """Find the containing time slot of the smaller time slot."""
@@ -93,8 +121,7 @@ class DeviceVolumeTimeSeries:
         return {
             "SSP": ssp_value,
             **{
-                f"{market_type}_{order_type}": 0
+                f"{market_type}": {"bought_kWh": 0, "sold_kWh": 0}
                 for market_type in FORWARD_MARKET_TYPES
-                for order_type in ["sold", "bought"]
             }
         }
