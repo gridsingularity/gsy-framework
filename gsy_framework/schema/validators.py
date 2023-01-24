47df17db034bb22486fb33455c8e0f8253b0b65c
Amirhossein <amirhossein@gridsingularity.com>
2023-01-24 10:27:05 +0100

    GSYE-452: Add SchemaError exception class

--- gsy_framework/schema/validators.py
+++ gsy_framework/schema/validators.py
@@ -8,11 +8,21 @@ from avro.io import validate as validate_avro_schema
 AVRO_SCHEMAS_PATH = Path(__file__).parent / "avro_schemas"
 
 
+class SchemaError(Exception):
+    """Exception class to be used by all defined schema validators."""
+    def __init__(self, message: str):
+        super().__init__(self)
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+
 class BaseSchemaValidator(abc.ABC):
     """Base schema validator class to control inter-service communications."""
 
     @abc.abstractmethod
-    def validate(self, data) -> (bool, str):
+    def validate(self, data, raise_exception: bool = False) -> (bool, str):
         """Validate given data using the defined schema."""
 
 
@@ -22,11 +32,13 @@ class AVROSchemaValidator(BaseSchemaValidator):
         with open(AVRO_SCHEMAS_PATH / f"{schema_name}.json") as schema_file:
             self.schema = avro.schema.parse(schema_file.read())
 
-    def validate(self, data):
+    def validate(self, data, raise_exception: bool = False) -> (bool, str):
         try:
             validate_avro_schema(self.schema, data, raise_on_error=True)
             return True, ""
         except AvroTypeException as exc:
+            if raise_exception:
+                raise SchemaError(message=f"The provided data is invalid for the schema.") from exc
             return False, str(exc)
 
 
