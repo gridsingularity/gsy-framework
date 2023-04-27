19be96af06a11d7d8b4718a2f2cc80c7b2dd72fc
hannesdiedrich <hannes@gridsingularity.com>
2023-04-27 08:27:33 +0200

    GSYE-564: Refactored ProfileValidator

--- gsy_framework/validators/profile_validator.py
+++ gsy_framework/validators/profile_validator.py
@@ -10,35 +10,43 @@ from gsy_framework.utils import convert_str_to_pendulum_in_dict
 class ProfileValidator:
     """Validate profiles"""
 
-    @classmethod
-    def validate(cls, profile: Dict[str, float], start_time: DateTime, end_time: DateTime,
+    def __init__(self, profile: Dict[str, float], start_time: DateTime, end_time: DateTime,
                  slot_length: timedelta):
-        """Validate if profile corresponds to the start_time, end_time and slot_length setting."""
         assert len(profile) > 0, "profile is empty"
-        ordered_profile = OrderedDict(convert_str_to_pendulum_in_dict(profile))
-        cls._validate_start_end_date(ordered_profile, start_time, end_time, slot_length)
-        cls._validate_slot_length(ordered_profile, slot_length)
+        self.profile = OrderedDict(convert_str_to_pendulum_in_dict(profile))
+        self.start_time = start_time
+        self.end_time = end_time
+        self.slot_length = slot_length
 
-    @staticmethod
-    def _validate_start_end_date(
-            profile: Dict[DateTime, float], start_time: DateTime, end_time: DateTime,
-            slot_length: timedelta):
-        profile_start_time = next(iter(profile))
-        profile_end_time = next(reversed(profile))
-        assert profile_start_time <= start_time, (
-            f"start_time is not valid {profile_start_time}, should be <= {start_time}")
-        if profile_end_time - profile_start_time == duration(days=1) - slot_length:
+    def validate(self):
+        """Validate if profile corresponds to the start_time, end_time and slot_length setting."""
+        self._validate_start_end_date()
+        self._validate_slot_length()
+
+    @property
+    def _profile_start_time(self) -> DateTime:
+        return next(iter(self.profile))
+
+    @property
+    def _profile_end_time(self) -> DateTime:
+        return next(reversed(self.profile))
+
+    def _validate_start_end_date(self):
+        assert self._profile_start_time <= self.start_time, (
+            f"start_time is not valid {self._profile_start_time}, should be <= {self.start_time}")
+        if (self._profile_end_time -
+                self._profile_start_time == duration(days=1) - self.slot_length):
             # if the profile is only one day long, it will be copied to other days and
             # consequently, the end_date does not have to be validated
             return
-        assert profile_end_time >= end_time, (
-            f"end_time is not valid {profile_end_time}, should be >= {end_time}")
+        assert self._profile_end_time >= self.end_time, (
+            f"end_time is not valid {self._profile_end_time}, should be >= {self.end_time}")
 
-    @staticmethod
-    def _validate_slot_length(profile: Dict[DateTime, float], slot_length: timedelta):
-        first_time = next(iter(profile))
-        for time_slot in list(profile.keys())[1:]:
-            assert first_time.add(seconds=int(slot_length.total_seconds())) == time_slot, (
+    def _validate_slot_length(self):
+        first_time = self._profile_start_time
+        for time_slot in list(self.profile.keys())[1:]:
+            assert first_time.add(seconds=int(self.slot_length.total_seconds())) == time_slot, (
                 f"Profile timestamp {time_slot} does not follow the slot length "
-                f"{slot_length.total_seconds() / 60} minutes (previous time_slot: {first_time})")
+                f"{self.slot_length.total_seconds() / 60} minutes "
+                f"(previous time_slot: {first_time})")
             first_time = time_slot
--- tests/test_validators/test_profile_validator.py
+++ tests/test_validators/test_profile_validator.py
@@ -11,33 +11,33 @@ class TestProfileValidator:
     @staticmethod
     @pytest.mark.parametrize("input_profile", [ONE_DAY_PROFILE, MULTI_DAY_PROFILE])
     def test_profile_validator_succeeds_when_valid_profile_provided(input_profile):
-        ProfileValidator.validate(
+        ProfileValidator(
             profile=input_profile,
             start_time=START_DATE,
             end_time=END_DATE,
-            slot_length=SLOT_LENGTH)
+            slot_length=SLOT_LENGTH).validate()
 
     @staticmethod
     @pytest.mark.parametrize("input_profile", [ONE_DAY_PROFILE, MULTI_DAY_PROFILE])
     def test_profile_validator_fails_if_start_date_is_not_respected(input_profile):
         del input_profile[next(iter(input_profile))]
         with pytest.raises(AssertionError):
-            ProfileValidator.validate(
+            ProfileValidator(
                 profile=input_profile,
                 start_time=START_DATE,
                 end_time=END_DATE,
-                slot_length=SLOT_LENGTH)
+                slot_length=SLOT_LENGTH).validate()
 
     @staticmethod
     @pytest.mark.parametrize("input_profile", [ONE_DAY_PROFILE, MULTI_DAY_PROFILE])
     def test_profile_validator_fails_if_end_date_is_not_respected(input_profile):
         del input_profile[next(reversed(input_profile))]
         with pytest.raises(AssertionError):
-            ProfileValidator.validate(
+            ProfileValidator(
                 profile=input_profile,
                 start_time=START_DATE,
                 end_time=END_DATE,
-                slot_length=SLOT_LENGTH)
+                slot_length=SLOT_LENGTH).validate()
 
     @staticmethod
     @pytest.mark.parametrize("input_profile", [ONE_DAY_PROFILE, MULTI_DAY_PROFILE])
@@ -46,11 +46,11 @@ class TestProfileValidator:
         wrong_time_stamp = format_datetime(START_DATE.add(minutes=SLOT_LENGTH.minutes / 5))
         input_profile[wrong_time_stamp] = 0.1
         with pytest.raises(AssertionError):
-            ProfileValidator.validate(
+            ProfileValidator(
                 profile=input_profile,
                 start_time=START_DATE,
                 end_time=END_DATE,
-                slot_length=SLOT_LENGTH)
+                slot_length=SLOT_LENGTH).validate()
 
     @staticmethod
     @pytest.mark.parametrize("input_profile", [ONE_DAY_PROFILE, MULTI_DAY_PROFILE])
@@ -58,8 +58,8 @@ class TestProfileValidator:
         gap_time = format_datetime(START_DATE.add(minutes=SLOT_LENGTH.minutes * 4))
         del input_profile[gap_time]
         with pytest.raises(AssertionError):
-            ProfileValidator.validate(
+            ProfileValidator(
                 profile=input_profile,
                 start_time=START_DATE,
                 end_time=END_DATE,
-                slot_length=SLOT_LENGTH)
+                slot_length=SLOT_LENGTH).validate()
