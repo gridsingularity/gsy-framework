969da3e365738a4ce57b035ebdf29bada445aba7
spyrostz <spyros@gridsingularity.com>
2023-06-15 17:24:55 +0200

    GSYE-612: Added teardown method to remove unneeded environment variables from the followup tests.

--- tests/test_redis_channels.py
+++ tests/test_redis_channels.py
@@ -5,6 +5,11 @@ from gsy_framework.redis_channels import QueueNames
 class TestRedisChannels:
 
     @staticmethod
+    def teardown_method():
+        os.environ.pop("LISTEN_TO_CANARY_NETWORK_REDIS_QUEUE", None)
+        os.environ.pop("LISTEN_TO_PAID_CUSTOMER_REDIS_QUEUE", None)
+
+    @staticmethod
     def test_gsy_e_queue_name():
         assert QueueNames().gsy_e_queue_name == "exchange"
         os.environ["LISTEN_TO_CANARY_NETWORK_REDIS_QUEUE"] = "no"
