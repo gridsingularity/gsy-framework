bef513e7878aca79edded2fcf6f5568ebfb4de53
spyrostz <spyros@gridsingularity.com>
2024-01-19 14:52:24 +0100

    Reverted one more change.

--- gsy_framework/read_user_profile.py
+++ gsy_framework/read_user_profile.py
@@ -297,8 +297,7 @@ def _hour_time_str(hour: float, minute: float) -> str:
     return f"{hour:02}:{minute:02}"
 
 
-def _copy_profile_to_multiple_days(
-        in_profile: Dict) -> Dict:
+def _copy_profile_to_multiple_days(in_profile: Dict) -> Dict:
     daytime_dict = dict(
         (_hour_time_str(time.hour, time.minute), time) for time in in_profile.keys())
     out_profile = {}
@@ -330,8 +329,7 @@ def read_arbitrary_profile(profile_type: InputProfileTypes,
                                                   current_timestamp=current_timestamp)
     profile_time_list = list(profile.keys())
     profile_duration = profile_time_list[-1] - profile_time_list[0]
-    if ((GlobalConfig.sim_duration > duration(days=1) and
-         GlobalConfig.sim_duration > profile_duration) or
+    if (GlobalConfig.sim_duration > duration(days=1) >= profile_duration or
             GlobalConfig.is_canary_network()):
         profile = _copy_profile_to_multiple_days(profile)
     if profile is not None:
