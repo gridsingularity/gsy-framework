9834b6e3e3279d60ecc48ce2204b5bf2934e2326
BigTava <bigtavadev@gmail.com>
2025-01-22 15:00:30 +0000

    GSYE-719: ensure correct merge between df_carbon_ratio and df_imported_energy and fix tz handling

--- gsy_framework/sim_results/carbon_emissions.py
+++ gsy_framework/sim_results/carbon_emissions.py
@@ -40,53 +40,55 @@ GENERATION_PLANT_TO_CARBON_EMISSIONS = {
 }
 
 
-GEOPY_TO_ENTSOE_COUNTRY_CODE = {
-    "AL": "AL",
-    "AD": "AD",
-    "AT": "AT",
-    "BY": "BY",
-    "BE": "BE",
-    "BA": "BA",
-    "BG": "BG",
-    "HR": "HR",
-    "CY": "CY",
-    "CZ": "CZ",
-    "DK": "DK",
-    "EE": "EE",
-    "FI": "FI",
-    "FR": "FR",
-    "DE": "DE_AMPRION",
-    "GR": "GR",
-    "HU": "HU",
-    "IS": "IS",
-    "IE": "IE",
-    "IT": "IT",
-    "XK": "XK",
-    "LV": "LV",
-    "LI": "LI",
-    "LT": "LT",
-    "LU": "LU",
-    "MT": "MT",
-    "MD": "MD",
-    "MC": "MC",
-    "ME": "ME",
-    "NL": "NL",
-    "MK": "MK",
-    "NO": "NO",
-    "PL": "PL",
-    "PT": "PT",
-    "RO": "RO",
-    "SM": "SM",
-    "RS": "RS",
-    "SK": "SK",
-    "SI": "SI",
-    "ES": "ES",
-    "SE": "SE",
-    "CH": "CH",
-    "UA": "UA",
-    "GB": "GB",
-    "VA": "VA",
-}
+GEOPY_TO_ENTSOE_COUNTRY_CODE = (
+    {  # see mappings https://github.com/EnergieID/entsoe-py/blob/master/entsoe/mappings.py
+        "AL": "AL",
+        "AD": "AD",
+        "AT": "AT",
+        "BY": "BY",
+        "BE": "BE",
+        "BA": "BA",
+        "BG": "BG",
+        "HR": "HR",
+        "CY": "CY",
+        "CZ": "CZ",
+        "DK": "DK",
+        "EE": "EE",
+        "FI": "FI",
+        "FR": "FR",
+        "DE": "DE_AMPRION",
+        "GR": "GR",
+        "HU": "HU",
+        "IS": "IS",
+        "IE": "IE",
+        "IT": "IT",
+        "XK": "XK",
+        "LV": "LV",
+        "LI": "LI",
+        "LT": "LT",
+        "LU": "LU",
+        "MT": "MT",
+        "MD": "MD",
+        "MC": "MC",
+        "ME": "ME",
+        "NL": "NL",
+        "MK": "MK",
+        "NO": "NO",
+        "PL": "PL",
+        "PT": "PT",
+        "RO": "RO",
+        "SM": "SM",
+        "RS": "RS",
+        "SK": "SK",
+        "SI": "SI",
+        "ES": "ES",
+        "SE": "SE",
+        "CH": "CH",
+        "UA": "UA",
+        "GB": "GB",
+        "VA": "VA",
+    }
+)
 
 MONTHLY_CARBON_EMISSIONS_COUNTRY_CODES = [
     "ES",
@@ -183,26 +185,30 @@ class EntsoePandasClientAdapter(EntsoePandasClient):
             raise ValueError("Invalid API key")
 
         df_generation = parse_generation(text, per_plant=True, include_eic=False)
+
+        # check if empty
         if df_generation.empty:
             raise ValueError("The Entsoe dataframe is empty. Please check the query parameters.")
+        # localize or covert to UTC+0
+        if df_generation.index.tzinfo is not None:
+            df_generation.index = df_generation.index.tz_convert("UTC")
+        else:
+            df_generation.index = df_generation.index.tz_localize("UTC")
+
+        # df_generation is a MultiIndex DataFrame
         df_generation.columns = df_generation.columns.set_levels(
             df_generation.columns.levels[0].str.encode("latin-1").str.decode("utf-8"), level=0
         )
-        df_generation = df_generation.tz_convert(area.tz)
-        # Truncation will fail if data is not sorted along the index in rare
-        # cases. Ensure the dataframe is sorted:
+        # Truncation will fail if data is not sorted along the index
         df_generation = df_generation.sort_index(axis=0)
-
         if df_generation.columns.nlevels == 2:
             df_generation = (
                 df_generation.assign(newlevel="Actual Aggregated")
                 .set_index("newlevel", append=True)
                 .unstack("newlevel")
             )
+        df_generation = df_generation.loc[start:end]
 
-        df_generation = df_generation.truncate(
-            before=start.tz_convert(area.tz), after=end.tz_convert(area.tz)
-        )
         return df_generation
 
 
@@ -231,6 +237,10 @@ class CarbonEmissionsHandler:
         (min, median, max)"""
         if stat not in ["min", "median", "max"]:
             raise ValueError("stat must be one of 'min', 'median', or 'max'")
+        if start.tzinfo is None or end.tzinfo is None:
+            raise ValueError("start and end must have timezone")
+        if start.utcoffset() != pd.Timedelta(0) or end.utcoffset() != pd.Timedelta(0):
+            raise ValueError("start and end must be in UTC+0")
 
         if (
             country_code in GEOPY_TO_ENTSOE_COUNTRY_CODE.keys()
@@ -257,7 +267,8 @@ class CarbonEmissionsHandler:
             index=df_generation_per_plant.columns,  # Map emissions to columns
         )
 
-        df_numeric = df_generation_per_plant.iloc[1:].apply(pd.to_numeric, errors="coerce")
+        df_numeric = df_generation_per_plant.iloc[0:].apply(pd.to_numeric, errors="coerce")
+
         df_numeric.index = pd.to_datetime(df_numeric.index, utc=True)
         df_numeric["Total Energy (kWh)"] = df_numeric.sum(axis=1) * 1000  # convert MWh to kWh
         df_numeric["Total Carbon (gCO2eq)"] = df_numeric.mul(emissions_map, axis=1).sum(axis=1)
@@ -267,7 +278,13 @@ class CarbonEmissionsHandler:
 
         # format columns to be only one level
         df_numeric.columns = ["_".join(filter(None, map(str, col))) for col in df_numeric.columns]
-        return df_numeric[["Ratio (gCO2eq/kWh)"]]
+
+        # fill df_carbon_ratio in case of missing hours using the last value
+        df_carbon_ratio = df_numeric[["Ratio (gCO2eq/kWh)"]]
+        df_carbon_ratio = df_carbon_ratio.reindex(
+            pd.date_range(start=start, end=end, freq="H", tz="UTC"), method="pad"
+        )
+        return df_carbon_ratio
 
     def calculate_from_carbon_ratio_and_imported_energy(
         self, df_carbon_ratio: pd.DataFrame, df_imported_energy: pd.DataFrame
@@ -279,9 +296,12 @@ class CarbonEmissionsHandler:
             raise ValueError(
                 "df_carbon_ratio must contain columns: simulation_time, Ratio (gCO2eq/kWh)"
             )
-        if not {"simulation_time", "imported_from_grid", "imported_from_community"}.issubset(
-            df_imported_energy.columns
-        ):
+        if not {
+            "simulation_time",
+            "imported_from_grid",
+            "imported_from_community",
+            "area_uuid",
+        }.issubset(df_imported_energy.columns):
             raise ValueError(
                 "df_imported_energy must contain columns: simulation_time, \
                 imported_from_grid, imported_from_community"
@@ -291,6 +311,24 @@ class CarbonEmissionsHandler:
         df_imported_energy["simulation_time"] = pd.to_datetime(
             df_imported_energy["simulation_time"], utc=True
         )
+        # Resample df_carbon_ratio to the frequency of df_imported_energy
+        freq = pd.infer_freq(
+            df_imported_energy["simulation_time"].dt.tz_localize(None).drop_duplicates()
+        )
+        if freq not in ["15T", "30T", "H"]:
+            raise ValueError(
+                "df_imported_energy must have a frequency of 15 minutes, 30 minutes, or 1 hour"
+            )
+        df_carbon_ratio = df_carbon_ratio.resample(freq).ffill()
+        full_range = pd.date_range(
+            start=df_carbon_ratio.index.min().floor("D"),
+            end=df_carbon_ratio.index.max().ceil("D") - pd.Timedelta(minutes=1),
+            freq=freq,
+            tz="UTC",
+        )
+        df_carbon_ratio = df_carbon_ratio.reindex(full_range, method="ffill")
+        df_carbon_ratio.bfill(inplace=True)
+
         df_combined = pd.merge(
             df_imported_energy,
             df_carbon_ratio,
@@ -310,6 +348,7 @@ class CarbonEmissionsHandler:
         df_combined = df_combined[
             [
                 "simulation_time",
+                "area_uuid",
                 "imported_from_community",
                 "imported_from_grid",
                 "carbon_generated_base_case_g",
