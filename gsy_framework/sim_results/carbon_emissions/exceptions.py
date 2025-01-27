f8b7d411e91f39d1cb10f8e6fdfcab3665bc56fc
BigTava <bigtavadev@gmail.com>
2025-01-27 23:44:48 +0000

    GSYE-719: small changes according to comments

---
+++ gsy_framework/sim_results/carbon_emissions/exceptions.py
@@ -0,0 +1,2 @@
+class EntsoeDataLimitExceededError(ValueError):
+    """Exception raised when the amount of requested data exceeds the allowed limit."""
--- tests/test_sim_results/test_carbon_emissions.py
+++ tests/test_sim_results/test_carbon_emissions.py
@@ -7,12 +7,17 @@ import pandas as pd
 from gsy_framework.constants_limits import (
     FLOATING_POINT_TOLERANCE,
 )
-from gsy_framework.sim_results.carbon_emissions import (
+from gsy_framework.sim_results.carbon_emissions.results import (
     CarbonEmissionsHandler,
-    MONTHLY_CARBON_EMISSIONS_COUNTRY_CODES,
 )
-
-from .constants import TEST_TRADE_PROFILE_RESULTS, TEST_IMPORTED_EXPORTED_ENERGY_RESULTS
+from gsy_framework.sim_results.carbon_emissions.constants import (
+    MONTHLY_CARBON_EMISSIONS_COUNTRY_CODES,
+    YEARLY_CARBON_EMISSIONS_COUNTRY_CODES,
+)
+from tests.test_sim_results.constants import (
+    TEST_TRADE_PROFILE_RESULTS,
+    TEST_IMPORTED_EXPORTED_ENERGY_RESULTS,
+)
 
 
 class TestCarbonEmissionsHandler:
@@ -23,9 +28,9 @@ class TestCarbonEmissionsHandler:
         self.imported_exported_energy = TEST_IMPORTED_EXPORTED_ENERGY_RESULTS
 
     @patch(
-        "gsy_framework.sim_results.carbon_emissions.EntsoePandasClientAdapter._query_generation_per_plant",  # noqa: E501
+        "gsy_framework.sim_results.carbon_emissions.results.EntsoePandasClientAdapter._query_generation_per_plant",  # noqa: E501
         lambda *args: pd.read_csv(
-            "tests/static/generation_per_plant_entsoe_BE_20210801.csv", header=[0, 1], index_col=0
+            "tests/static/generation_per_plant_entsoe_BE_20240101.csv", header=[0, 1], index_col=0
         ),
     )
     def test_carbon_emissions_from_gsy_trade_profile_calculates_correctly(
@@ -45,9 +50,9 @@ class TestCarbonEmissionsHandler:
         )
 
     @patch(
-        "gsy_framework.sim_results.carbon_emissions.EntsoePandasClientAdapter._query_generation_per_plant",  # noqa: E501
+        "gsy_framework.sim_results.carbon_emissions.results.EntsoePandasClientAdapter._query_generation_per_plant",  # noqa: E501
         lambda *args: pd.read_csv(
-            "tests/static/generation_per_plant_entsoe_BE_20210801.csv", header=[0, 1], index_col=0
+            "tests/static/generation_per_plant_entsoe_BE_20240101.csv", header=[0, 1], index_col=0
         ),
     )
     def test_carbon_emissions_from_gsy_imported_exported_energy_calculates_correctly(
@@ -79,35 +84,131 @@ class TestCarbonEmissionsHandler:
         )
 
     @patch(
-        "gsy_framework.sim_results.carbon_emissions.EntsoePandasClientAdapter._query_generation_per_plant",  # noqa: E501
+        "gsy_framework.sim_results.carbon_emissions.results.EntsoePandasClientAdapter._query_generation_per_plant",  # noqa: E501
         lambda *args: pd.read_csv(
-            "tests/static/generation_per_plant_entsoe_BE_20210801.csv", header=[0, 1], index_col=0
+            "tests/static/generation_per_plant_entsoe_BE_20240101.csv", header=[0, 1], index_col=0
         ),
     )
     @pytest.mark.parametrize(
-        "country_code",
-        MONTHLY_CARBON_EMISSIONS_COUNTRY_CODES
-        + [  # Monthly + Yearly Country codes
-            "ET",
-            "GF",
-            "PF",
-            "GH",
-            "GI",
-            "GL",
-            "GD",
-            "GP",
-            "GU",
-            "GT",
-            "GN",
-            "GW",
-            "GY",
-            "HT",
-            "HN",
-            "IR",
-            "IQ",
-        ],
+        ("country_code", "expected_carbon_generated_g"),
+        list(
+            zip(
+                MONTHLY_CARBON_EMISSIONS_COUNTRY_CODES + YEARLY_CARBON_EMISSIONS_COUNTRY_CODES,
+                [
+                    1275.092000,
+                    152.852000,
+                    170.632000,
+                    301.420000,
+                    57.064000,
+                    475.244000,
+                    1008.000000,
+                    1624.504000,
+                    1296.568000,
+                    2050.300000,
+                    1148.252000,
+                    1092.280000,
+                    294.084000,
+                    216.160000,
+                    301.140000,
+                    286.048000,
+                    1504.356000,
+                    1031.828000,
+                    991.956000,
+                    1180.704000,
+                    958.132000,
+                    1.988000,
+                    1784.272000,
+                    371.084336,
+                    1776.910800,
+                    1711.111080,
+                    489.256208,
+                    1711.111080,
+                    991.488036,
+                    740.706820,
+                    1571.428600,
+                    1536.338328,
+                    1879.889312,
+                    1848.276080,
+                    2532.920600,
+                    1935.951360,
+                    1695.412880,
+                    632.258088,
+                    1635.398240,
+                    1822.222192,
+                    65.333335,
+                    1488.724328,
+                    2374.144360,
+                    152.852000,
+                    1811.764640,
+                    2502.956400,
+                    1309.091000,
+                    700.000056,
+                    1169.579936,
+                    855.172360,
+                    476.119028,
+                    1562.790600,
+                    1799.999880,
+                    0.000000,
+                    1759.999920,
+                    815.117800,
+                    1630.487488,
+                    726.631276,
+                    1800.000160,
+                    1960.000000,
+                    700.000000,
+                    57.064000,
+                    1102.877720,
+                    1785.306880,
+                    68.478267,
+                    1938.461840,
+                    1482.353040,
+                    1626.183720,
+                    1866.666760,
+                    420.625912,
+                    1596.855512,
+                    760.110876,
+                    1657.142844,
+                    1768.420920,
+                    482.758612,
+                    69.001290,
+                    1400.000000,
+                    1133.333404,
+                    807.692424,
+                    609.900984,
+                    1240.000020,
+                    1376.470536,
+                    1866.666760,
+                    469.262892,
+                    1355.200084,
+                    1680.000168,
+                    500.000004,
+                    1792.000000,
+                    1400.000000,
+                    1744.000160,
+                    919.149056,
+                    663.157936,
+                    1750.000000,
+                    1792.982520,
+                    1588.461560,
+                    790.341356,
+                    1008.000000,
+                    1997.633960,
+                    1624.504000,
+                    1834.349832,
+                    1928.679088,
+                    1555.555512,
+                    1148.252000,
+                    1514.586220,
+                    2299.894520,
+                    197.377057,
+                    1866.666760,
+                ],
+            )
+        ),
     )
-    def test_carbon_emissions_handles_different_country_codes_correctly(self, country_code):
+    def test_carbon_emissions_handles_different_country_codes_correctly(
+        self, country_code, expected_carbon_generated_g
+    ):
         # Given
         energy_handler = CarbonEmissionsHandler(entsoe_api_key="false_key")
 
@@ -117,4 +218,8 @@ class TestCarbonEmissionsHandler:
         )
 
         # Then
-        assert carbon_emissions["carbon_generated_g"] > 0
+        assert isclose(
+            carbon_emissions["carbon_generated_g"],
+            expected_carbon_generated_g,
+            abs_tol=FLOATING_POINT_TOLERANCE,
+        )
