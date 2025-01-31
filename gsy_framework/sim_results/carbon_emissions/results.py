3c66a2b754e3609b7af9480af3d7aa0379374961
BigTava <bigtavadev@gmail.com>
2025-01-31 18:17:34 +0000

    GSYE-846: move query from Entsoye logic to gsy-web

--- gsy_framework/sim_results/carbon_emissions/results.py
+++ gsy_framework/sim_results/carbon_emissions/results.py
@@ -1,92 +1,16 @@
-from typing import Union, Dict, List, Tuple
+from typing import Dict, List, Tuple
 from datetime import datetime
 
 import pytz
 
-import requests
 import pandas as pd
-from entsoe import EntsoePandasClient
-from entsoe.mappings import lookup_area, Area
-from entsoe.parsers import parse_generation
 from geopy.geocoders import Nominatim
 from iso3166 import countries
 
 from gsy_framework.constants_limits import DATE_TIME_FORMAT
 from gsy_framework.sim_results.carbon_emissions.constants import (
-    ENTSOE_URL,
-    GENERATION_PLANT_TO_CARBON_EMISSIONS,
-    GEOPY_TO_ENTSOE_COUNTRY_CODE,
     MONTHLY_CARBON_EMISSIONS_COUNTRY_CODES,
-    Stat,
 )
-from gsy_framework.sim_results.carbon_emissions.exceptions import EntsoeDataLimitExceededError
-
-
-class EntsoePandasClientAdapter(EntsoePandasClient):
-    def __init__(self, api_key: str = None):
-        super().__init__(api_key=api_key)
-
-    def _base_request(
-        self, params: Dict, start: pd.Timestamp, end: pd.Timestamp
-    ) -> requests.Response:
-        """Overwrites EntsoePandasClient _base_request method"""
-        start_str = self._datetime_to_str(start)
-        end_str = self._datetime_to_str(end)
-        base_params = {
-            "securityToken": self.api_key,
-            "periodStart": start_str,
-            "periodEnd": end_str,
-        }
-        params.update(base_params)
-        response = self.session.get(url=ENTSOE_URL, params=params, proxies=None, timeout=None)
-        return response
-
-    def _query_generation_per_plant(
-        self, country_code: Union[Area, str], start: pd.Timestamp, end: pd.Timestamp
-    ):
-        """Overwrites EntsoePandasClient query_generation_per_plant method"""
-        area = lookup_area(country_code)
-        params = {
-            "documentType": "A73",
-            "processType": "A16",
-            "in_Domain": area.code,
-        }
-        response = self._base_request(params=params, start=start, end=end)
-        text = response.text
-
-        if "Unauthorized" in text:
-            raise ValueError("Invalid API key")
-        if "The amount of requested data exceeds allowed limit." in text:
-            raise EntsoeDataLimitExceededError(
-                "The amount of requested data exceeds allowed limit."
-            )
-
-        df_generation = parse_generation(text, per_plant=True, include_eic=False)
-
-        # check if empty
-        if df_generation.empty:
-            raise ValueError("The Entsoe dataframe is empty. Please check the query parameters.")
-        # localize or covert to UTC+0
-        if df_generation.index.tzinfo is not None:
-            df_generation.index = df_generation.index.tz_convert("UTC")
-        else:
-            df_generation.index = df_generation.index.tz_localize("UTC")
-
-        # df_generation is a MultiIndex DataFrame
-        df_generation.columns = df_generation.columns.set_levels(
-            df_generation.columns.levels[0].str.encode("latin-1").str.decode("utf-8"), level=0
-        )
-        # Truncation will fail if data is not sorted along the index
-        df_generation = df_generation.sort_index(axis=0)
-        if df_generation.columns.nlevels == 2:
-            df_generation = (
-                df_generation.assign(newlevel="Actual Aggregated")
-                .set_index("newlevel", append=True)
-                .unstack("newlevel")
-            )
-        df_generation = df_generation.loc[start:end]
-
-        return df_generation
 
 
 class CarbonEmissionsHandler:
@@ -95,8 +19,8 @@ class CarbonEmissionsHandler:
     from https://github.com/EnergieID/entsoe-py (branch v0.4.4) to make this work.
     """
 
-    def __init__(self, entsoe_api_key: str = None):
-        self.entsoe_api_key = entsoe_api_key
+    def __init__(self):
+        pass
 
     def coordinates_to_country_code(self, lat: float, lon: float) -> str:
         """Get the country code based on the coordinates"""
@@ -117,63 +41,17 @@ class CarbonEmissionsHandler:
         end_date = max(dates).replace(tzinfo=pytz.UTC).replace(minute=0, second=0, microsecond=0)
         return start_date, end_date
 
-    def _query_from_entsoe(
-        self, country_code: str, start: pd.Timestamp, end: pd.Timestamp, stat: Stat = Stat.MEDIAN
-    ):
-        """Query generation per plant from the Entsoe API"""
-        entsoe_client = EntsoePandasClientAdapter(api_key=self.entsoe_api_key)
-        country_code = GEOPY_TO_ENTSOE_COUNTRY_CODE[country_code]
-        df_generation_per_plant = entsoe_client._query_generation_per_plant(
-            country_code, start, end
-        )  # example: https://transparency.entsoe.eu/generation/r2/actualGenerationPerGenerationUnit/show?name=&defaultValue=true&viewType=TABLE&areaType=BZN&atch=false&dateTime.dateTime=01.01.2024+00:00|UTC|DAYTIMERANGE&dateTime.endDateTime=01.01.2024+00:00|UTC|DAYTIMERANGE&area.values=CTY|10YSE-1--------K!BZN|10Y1001A1001A44P&masterDataFilterName=&masterDataFilterCode=&productionType.values=B01&productionType.values=B25&productionType.values=B02&productionType.values=B03&productionType.values=B04&productionType.values=B05&productionType.values=B06&productionType.values=B07&productionType.values=B08&productionType.values=B09&productionType.values=B10&productionType.values=B11&productionType.values=B12&productionType.values=B13&productionType.values=B14&productionType.values=B20&productionType.values=B15&productionType.values=B16&productionType.values=B17&productionType.values=B18&productionType.values=B19&dateTime.timezone=UTC&dateTime.timezone_input=UTC&dv-datatable_length=50  # noqa
-
-        # df_generation_per_plant is a MultiIndex DataFrame
-        generation_types = df_generation_per_plant.columns.get_level_values(1)
-        emissions_map = pd.Series(
-            [
-                GENERATION_PLANT_TO_CARBON_EMISSIONS[generation_type][stat]
-                for generation_type in generation_types
-            ],
-            index=df_generation_per_plant.columns,  # Map emissions to columns
-        )
-        df_numeric = df_generation_per_plant.iloc[1:].apply(pd.to_numeric, errors="coerce")
-        df_numeric.index = pd.to_datetime(df_numeric.index, utc=True)
-
-        df_numeric["Total Energy (kWh)"] = df_numeric.sum(axis=1)  # convert from MW to kWh
-        df_numeric["Total Carbon (gCO2eq)"] = df_numeric.mul(emissions_map, axis=1).sum(axis=1)
-        df_numeric["Ratio (gCO2eq/kWh)"] = (
-            df_numeric["Total Carbon (gCO2eq)"] / df_numeric["Total Energy (kWh)"]
-        )
-
-        # format columns to be only one level
-        df_numeric.columns = ["_".join(filter(None, map(str, col))) for col in df_numeric.columns]
-
-        return df_numeric
-
     def calculate_carbon_ratio(
-        self, country_code: str, start: pd.Timestamp, end: pd.Timestamp, stat: Stat = Stat.MEDIAN
+        self, country_code: str, start: datetime, end: datetime
     ) -> pd.DataFrame:
-        """Calculate the total carbon generated based on the specified statistic
-        (min, median, max)"""
+        """Calculate the total carbon generated based on the specified statistic"""
+
         if start.tzinfo is None or end.tzinfo is None:
             raise ValueError("start and end must have timezone")
         if start.utcoffset() != pd.Timedelta(0) or end.utcoffset() != pd.Timedelta(0):
             raise ValueError("start and end must be in UTC+0")
 
         if (
-            country_code in GEOPY_TO_ENTSOE_COUNTRY_CODE
-        ):  # source: https://transparency.entsoe.eu/)
-            try:
-                df_carbon_ratio = self._query_from_entsoe(country_code, start, end, stat)
-            except EntsoeDataLimitExceededError:
-                df_carbon_ratio = pd.DataFrame()
-                days_range = pd.date_range(start, end, freq="H")
-                for day in days_range.to_period("D").unique():
-                    df_daily = self._query_from_entsoe(
-                        country_code, day.start_time, day.end_time, stat
-                    )
-                    df_carbon_ratio = pd.concat([df_carbon_ratio, df_daily])
-        elif (
             country_code in MONTHLY_CARBON_EMISSIONS_COUNTRY_CODES
         ):  # source: https://www.electricitymaps.com/data-portal
             file_path = (
@@ -212,50 +90,49 @@ class CarbonEmissionsHandler:
         return df_carbon_ratio
 
     def calculate_from_carbon_ratio_and_imported_energy(
-        self, df_carbon_ratio: pd.DataFrame, df_imported_energy: pd.DataFrame
+        self, carbon_ratio: Dict, imported_energy: Dict
     ):
-        """Calculate the carbon emissions from a dataframe with carbon ratios
+        """Calculate the carbon emissions from a dict with carbon ratios
         and another with imported/exported energy."""
-        if not {"Ratio (gCO2eq/kWh)"}.issubset(df_carbon_ratio.columns):
+
+        if not all("Ratio (gCO2eq/kWh)" in obj for obj in carbon_ratio):
+            raise ValueError("carbon_ratio must contain 'Ratio (gCO2eq/kWh)' in each entry")
+
+        if not all(
+            {
+                "simulation_time",
+                "area_uuid",
+                "imported_from_grid",
+                "imported_from_community",
+            }.issubset(obj)
+            for obj in imported_energy
+        ):
             raise ValueError(
-                "df_carbon_ratio must contain columns: simulation_time, Ratio (gCO2eq/kWh)"
-            )
-        if not {
-            "simulation_time",
-            "area_uuid",
-            "imported_from_grid",
-            "imported_from_community",
-        }.issubset(df_imported_energy.columns):
-            raise ValueError(
-                "df_imported_energy must contain columns: simulation_time, \
-                imported_from_grid, imported_from_community"
+                "Each entry in imported_energy must contain keys: ",
+                "simulation_time, area_uuid, imported_from_grid, imported_from_community",
             )
 
-        df_carbon_ratio.index = pd.to_datetime(df_carbon_ratio.index)
-        df_imported_energy["simulation_time"] = pd.to_datetime(
-            df_imported_energy["simulation_time"], utc=True
-        )
+        # Find the carbon ratio closest to the simulation time
+        carbon_ratio_sorted = sorted(carbon_ratio, key=lambda x: x["time"])
+        for obj in imported_energy:
+            simluation_time = obj["simulation_time"]
+            obj["carbon_ratio"] = min(
+                carbon_ratio_sorted, key=lambda x: abs(x["time"] - simluation_time)
+            )["Ratio (gCO2eq/kWh)"]
 
-        # Find the nearest ratio for each df_imported_energy timestamp
-        df_imported_energy["carbon_ratio"] = df_imported_energy["simulation_time"].apply(
-            lambda ts: df_carbon_ratio.iloc[
-                df_carbon_ratio.index.get_indexer([ts], method="nearest")[0]
-            ]
-        )
-        df_imported_energy["carbon_generated_base_case_g"] = (
-            df_imported_energy["imported_from_community"]
-            + df_imported_energy["imported_from_grid"]
-        ) * df_imported_energy["carbon_ratio"]
+        # Calculate carbon emissions
+        for obj in imported_energy:
+            obj["carbon_generated_base_case_g"] = (
+                obj["imported_from_community"] + obj["imported_from_grid"]
+            ) * obj["carbon_ratio"]
 
-        df_imported_energy["carbon_generated_gsy_g"] = (
-            df_imported_energy["imported_from_grid"] * df_imported_energy["carbon_ratio"]
-        )
+            obj["carbon_generated_gsy_g"] = obj["imported_from_grid"] * obj["carbon_ratio"]
 
-        df_imported_energy["carbon_savings_g"] = (
-            df_imported_energy["carbon_generated_base_case_g"]
-            - df_imported_energy["carbon_generated_gsy_g"]
-        )
-        return df_imported_energy
+            obj["carbon_savings_g"] = (
+                obj["carbon_generated_base_case_g"] - obj["carbon_generated_gsy_g"]
+            )
+
+        return imported_energy
 
     def calculate_from_gsy_trade_profile(self, country_code: str, trade_profile: Dict) -> Dict:
         """Calculate the carbon emissions from gsy-e trade profile,
