20ce91b1c1d96327820e8397591abc92ebdf46a2
hannesdiedrich <hannes@gridsingularity.com>
2025-04-09 11:04:57 +0200

    GSYE-498: Adapt the profile validator to return specific dateteme formats when returning errors in order to fix unit tests on the gsy-web side

--- gsy_framework/validators/profile_validator.py
+++ gsy_framework/validators/profile_validator.py
@@ -3,14 +3,19 @@ from datetime import timedelta
 from typing import Dict, Optional
 
 from pendulum import DateTime, duration
+from gsy_framework.constants_limits import DATE_TIME_FORMAT
 
 
 class ProfileValidator:
     """Validate profiles"""
 
     def __init__(
-            self, profile: Dict[DateTime, float], start_time: Optional[DateTime] = None,
-            end_time: Optional[DateTime] = None, slot_length: Optional[timedelta] = None):
+        self,
+        profile: Dict[DateTime, float],
+        start_time: Optional[DateTime] = None,
+        end_time: Optional[DateTime] = None,
+        slot_length: Optional[timedelta] = None,
+    ):
 
         self.profile = OrderedDict(profile)
         if not self.profile:
@@ -39,29 +44,38 @@ class ProfileValidator:
 
     def _validate_start_end_date(self):
         assert self._profile_start_time <= self.start_time, (
-            f"Profile start time {self._profile_start_time} is not consistent with the simulation "
-            f"start time {self.start_time}.")
-        if (self._profile_end_time -
-                self._profile_start_time == duration(days=1) - self.slot_length):
+            f"Profile start time {self._profile_start_time.format(DATE_TIME_FORMAT)} "
+            f"is not consistent with the simulation "
+            f"start time {self.start_time.format(DATE_TIME_FORMAT)}."
+        )
+        if (
+            self._profile_end_time - self._profile_start_time
+            == duration(days=1) - self.slot_length
+        ):
             # if the profile is only one day long, it will be copied to other days and
             # consequently, the end_date does not have to be validated
             return
         assert self._profile_end_time >= self.end_time, (
-            f"Profile end time {self._profile_end_time} is not consistent with the simulation "
-            f"end time {self.end_time}.")
+            f"Profile end time {self._profile_end_time.format(DATE_TIME_FORMAT)} "
+            f"is not consistent with the simulation "
+            f"end time {self.end_time.format(DATE_TIME_FORMAT)}."
+        )
 
     def _validate_slot_length(self):
         first_time = self._profile_start_time
         for time_slot in list(self.profile.keys())[1:]:
             assert first_time.add(seconds=int(self.slot_length.total_seconds())) == time_slot, (
-                f"Profile timestamp {time_slot} does not follow the slot length "
+                f"Profile timestamp {time_slot.format(DATE_TIME_FORMAT)} "
+                f"does not follow the slot length "
                 f"{self.slot_length.total_seconds() / 60} minutes "
-                f"(previous time_slot: {first_time})")
+                f"(previous time_slot: {first_time.format(DATE_TIME_FORMAT)})"
+            )
             first_time = time_slot
 
     def _get_and_validate_time_diffs(self) -> timedelta:
         timestamps = list(self.profile.keys())
         time_diffs = {
-            (end - start).total_seconds() for start, end in zip(timestamps[:-1], timestamps[1:])}
+            (end - start).total_seconds() for start, end in zip(timestamps[:-1], timestamps[1:])
+        }
         assert len(time_diffs) == 1
         return timedelta(seconds=next(iter(time_diffs)))
